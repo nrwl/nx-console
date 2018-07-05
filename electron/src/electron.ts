fc0ffdfde6ddc390d1354e2e12627db767eda043
Victor Savkin <vic.savkin@gmail.com>
2018-07-04 20:35:53 -0400

    feat: create command-line launcher on mac

--- electron/src/electron.ts
+++ electron/src/electron.ts
@@ -1,22 +1,25 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 import { spawn } from 'child_process';
-import { statSync } from 'fs';
+import { statSync, writeFileSync } from 'fs';
 import * as fixPath from 'fix-path';
 import * as getPort from 'get-port';
+import * as os from 'os';
 
 let win;
 let p;
 
 fixPath();
 
+const currentDirectory = process.cwd();
+
 function createWindow() {
   win = new BrowserWindow({ width: 1400, height: 1200 });
 
   getPort({port: 7777}).then(port => {
     startServer(port).then(() => {
-      if (fileExists(path.join(process.cwd(), 'angular.json'))) {
-        win.loadURL(`http://localhost:${port}/workspaces/${encodeURIComponent(process.cwd())}/details`);
+      if (fileExists(path.join(currentDirectory, 'angular.json'))) {
+        win.loadURL(`http://localhost:${port}/workspaces/${encodeURIComponent(currentDirectory)}/details`);
       } else {
         win.loadURL(`http://localhost:${port}`);
       }
@@ -32,7 +35,20 @@ function createWindow() {
   });
 }
 
+createCommandLineLauncher();
 app.on('ready', createWindow);
+
+function createCommandLineLauncher() {
+  if (os.platform() === "win32") {
+    // TODO: implement this
+  } else if (os.platform() === "linux") {
+    // TODO: implement this
+  } else {
+    writeFileSync('/usr/local/bin/angular-console', `#!/bin/bash
+/Applications/AngularConsole.app/Contents/MacOS/AngularConsole &>/dev/null &
+`);
+  }
+}
 
 function startServer(port: number) {
   p = spawn('node', ['index.js', port.toString()], {stdio: [0, 1, 2], cwd: path.join(__dirname, 'server'), shell: true});
