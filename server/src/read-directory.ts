3141e3a9f978581a785691725166ecaa4d0c8b04
Victor Savkin <vic.savkin@gmail.com>
2018-07-29 11:25:26 -0400

    fix: fix finder on windows

--- server/src/read-directory.ts
+++ server/src/read-directory.ts
@@ -1,35 +1,37 @@
 import { directoryExists, fileExists } from './utils';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as os from 'os';
 
 export function readDirectory(
-  dirName: string,
+  _dirName: string,
   onlyDirectories: boolean,
   showHidden: boolean
 ) {
-  if (dirName && !directoryExists(dirName)) {
+  const dirName =
+    _dirName !== '' ? _dirName : os.platform() === 'win32' ? 'C:' : '/';
+  if (!directoryExists(dirName)) {
     throw new Error(`Cannot find directory: '${dirName}'`);
   }
-
-  if (!dirName) {
-    dirName = '/';
-  }
-
   const files = fs
     .readdirSync(dirName)
     .map(c => {
       const child = path.join(dirName, c);
-      if (fs.statSync(child).isDirectory()) {
-        if (fileExists(path.join(dirName, c, 'angular.json'))) {
-          return { name: c, type: 'angularDirectory' };
+      try {
+        if (fs.statSync(child).isDirectory()) {
+          if (fileExists(path.join(dirName, c, 'angular.json'))) {
+            return { name: c, type: 'angularDirectory' };
+          } else {
+            return { name: c, type: 'directory' };
+          }
         } else {
-          return { name: c, type: 'directory' };
+          return { name: c, type: 'file' };
         }
-      } else {
-        return { name: c, type: 'file' };
-      }
+      } catch (e) {}
     })
     .filter(t => {
+      if (!t) return false;
+
       let show = true;
       if (onlyDirectories && t.type === 'file') {
         show = false;
@@ -39,7 +41,7 @@ export function readDirectory(
       }
       return show;
     })
-    .map(t => {
+    .map((t: any) => {
       const hasChildren =
         fs.readdirSync(`${dirName}/${t.name}`).filter(child => {
           try {
