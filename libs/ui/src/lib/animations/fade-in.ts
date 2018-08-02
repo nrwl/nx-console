50a8e28601589b0b266fe5ad9872fce0e4aa6fc4
mrmeku <mrmeku@gmail.com>
2018-08-02 11:23:23 -0600

    feat(ui): Add splashes of color where appropriate

--- libs/ui/src/lib/animations/fade-in.ts
+++ libs/ui/src/lib/animations/fade-in.ts
@@ -5,7 +5,8 @@ import {
   AnimationQueryMetadata
 } from '@angular/animations';
 
-export const FADE_IN: AnimationQueryMetadata = query(':enter', [
-  style({ opacity: 0 }),
-  animate('300ms ease-in-out', style({ opacity: 1 }))
-]);
+export const FADE_IN: AnimationQueryMetadata = query(
+  ':enter',
+  [style({ opacity: 0 }), animate('300ms ease-in-out', style({ opacity: 1 }))],
+  { optional: true }
+);
