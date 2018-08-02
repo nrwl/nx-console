eed87dbd01d66bb93d53f56d072cf11c70446cc3
mrmeku <mrmeku@gmail.com>
2018-08-02 11:23:23 -0600

    feat(ui): Add splashes of color where appropriate

--- libs/feature-workspaces/src/lib/workspaces/workspaces.component.ts
+++ libs/feature-workspaces/src/lib/workspaces/workspaces.component.ts
@@ -1,12 +1,28 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Settings } from '@angular-console/utils';
+import { Settings, WorkspaceDescription } from '@angular-console/utils';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
 import { WorkspacesService } from '../workspaces.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'angular-console-workspaces',
   templateUrl: './workspaces.component.html',
-  styleUrls: ['./workspaces.component.scss']
+  styleUrls: ['./workspaces.component.scss'],
+  animations: [
+    trigger('growShrink', [
+      state('void', style({ height: 0 })),
+      state('*', style({ height: '*' })),
+      transition(`:enter`, animate(`250ms ease-in-out`)),
+      transition(`:leave`, animate(`250ms ease-in-out`))
+    ])
+  ]
 })
 export class WorkspacesComponent {
   constructor(
@@ -16,5 +32,9 @@ export class WorkspacesComponent {
 
   clearRecent() {
     this.settings.clear();
+  }
+
+  trackByPath(_: number, w: WorkspaceDescription) {
+    return w.path;
   }
 }
