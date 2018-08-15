5348be030541648afb0a4cb12eac74fcd05cef2c
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-08-15 22:01:26 +0200

    Use APOLLO_OPTIONS

--- apps/angular-console/src/app/app.module.ts
+++ apps/angular-console/src/app/app.module.ts
@@ -18,12 +18,48 @@ import {
   CancelCommandGuard,
   Messenger
 } from '@angular-console/utils';
-import { Apollo, ApolloModule } from 'apollo-angular';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 
 import { AppComponent } from './app.component';
+
+export function initApollo(
+  analytics: AnalyticsCollector,
+  messenger: Messenger,
+  httpLink: HttpLink
+) {
+  analytics.setUpRouterLogging();
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message }) => {
+        messenger.error(message);
+        analytics.reportException(message);
+      });
+    } else if (networkError) {
+      const n: any = networkError;
+      if (n.error && n.error.errors && n.error.errors.length > 0) {
+        const message = n.error.errors[0].message;
+        messenger.error(message);
+        analytics.reportException(message);
+      } else {
+        messenger.error(n.message);
+        analytics.reportException(n.message);
+      }
+    }
+  });
+
+  return {
+    defaultOptions: {
+      query: { fetchPolicy: 'network-only' },
+      watchQuery: { fetchPolicy: 'network-only' }
+    },
+    link: errorLink.concat(httpLink.create({})),
+    cache: new InMemoryCache()
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -49,46 +85,19 @@ import { AppComponent } from './app.component';
       { paramsInheritanceStrategy: 'always' }
     )
   ],
-  providers: [CancelCommandGuard, AnalyticsCollector],
+  providers: [
+    CancelCommandGuard,
+    AnalyticsCollector,
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: initApollo,
+      deps: [AnalyticsCollector, Messenger, HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(
-    analytics: AnalyticsCollector,
-    apollo: Apollo,
-    httpLink: HttpLink,
-    matIconRegistry: MatIconRegistry,
-    messenger: Messenger
-  ) {
-    analytics.setUpRouterLogging();
-
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        graphQLErrors.forEach(({ message }) => {
-          messenger.error(message);
-          analytics.reportException(message);
-        });
-      } else if (networkError) {
-        const n: any = networkError;
-        if (n.error && n.error.errors && n.error.errors.length > 0) {
-          const message = n.error.errors[0].message;
-          messenger.error(message);
-          analytics.reportException(message);
-        } else {
-          messenger.error(n.message);
-          analytics.reportException(n.message);
-        }
-      }
-    });
-
-    apollo.create({
-      defaultOptions: {
-        query: { fetchPolicy: 'network-only' },
-        watchQuery: { fetchPolicy: 'network-only' }
-      },
-      link: errorLink.concat(httpLink.create({})),
-      cache: new InMemoryCache()
-    });
+  constructor(matIconRegistry: MatIconRegistry) {
     matIconRegistry.setDefaultFontSetClass('material-icons-extended');
   }
 }
