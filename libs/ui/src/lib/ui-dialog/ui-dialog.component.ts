9f4799b011b4e3b45bd8a5a6f7aff38c9203d1fd
Wassim Chegham <github@wassim.app>
2018-11-21 17:32:42 +0100

    fix: revert the ViewEncapsulation.None on DialogComponent
    This is necessary to style the inner content of the mat-card > mat-card-content MD component.

--- libs/ui/src/lib/ui-dialog/ui-dialog.component.ts
+++ libs/ui/src/lib/ui-dialog/ui-dialog.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ui-dialog',
   templateUrl: './ui-dialog.component.html',
-  styleUrls: ['./ui-dialog.component.scss']
+  styleUrls: ['./ui-dialog.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DialogComponent {}
