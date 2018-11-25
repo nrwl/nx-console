926d2c4402af439e7f6d46c7b559b32429c99411
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-25 13:24:12 +0100

    Use schema.graphql

--- server/src/schema/schema.ts
+++
@@ -1,7 +0,0 @@
-import { buildSchema } from 'graphql';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-
-export const schema = buildSchema(
-  readFileSync(resolve(__dirname, './schema.graphql'), { encoding: 'utf-8' })
-);
