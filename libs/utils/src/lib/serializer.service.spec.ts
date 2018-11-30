765dc6632d439909a874ea61c0ee14582f86cc9c
Adam Medford <adammedford@gmail.com>
2018-11-30 10:16:57 -0600

    fix: add regression test for important fields
    Regression test to ensure that required and positional fields are marked important
    Test to make sure default values for important are ignored and instead set based on the other properties
    Test to make sure periods are stripped from the end of schematic descriptions

--- libs/utils/src/lib/serializer.service.spec.ts
+++ libs/utils/src/lib/serializer.service.spec.ts
@@ -1,3 +1,4 @@
+import { Schematic, Field } from '@angular-console/schema';
 import { Serializer } from './serializer.service';
 
 describe('Serializer', () => {
@@ -25,6 +26,66 @@ describe('Serializer', () => {
         ]
       );
       expect(serialized).toEqual(['--', '--one=two']);
+    });
+  });
+
+  describe('normalizeSchematic', () => {
+    const schematic: Schematic = {
+      collection: '@nrwl/testing',
+      name: 'normalized',
+      description: 'a schematic to be normalized.',
+      schema: [
+        {
+          type: 'arguments',
+          name: 'required',
+          enum: [],
+          description: '',
+          defaultValue: null,
+          required: true,
+          positional: false,
+          important: false
+        },
+        {
+          type: 'arguments',
+          name: 'positional',
+          enum: [],
+          description: '',
+          defaultValue: null,
+          required: false,
+          positional: true,
+          important: false
+        },
+        {
+          type: 'arguments',
+          name: 'ignoreImportant',
+          enum: [],
+          description: '',
+          defaultValue: null,
+          required: false,
+          positional: false,
+          important: true
+        }
+      ]
+    };
+    let normalized: Schematic;
+    it('should normalize the schematic, removing periods from the end of description', () => {
+      normalized = serializer.normalizeSchematic(schematic);
+      expect(normalized.description.lastIndexOf('.')).toBeLessThan(
+        normalized.description.length
+      );
+    });
+    it('should recognize positional and required fields as important but ignore previous values of important', () => {
+      normalized.schema.forEach((field: Field) => {
+        switch (field.name) {
+          case 'required':
+          case 'positional':
+            expect(field.important).toEqual(true);
+            break;
+          case 'ignoreImportant':
+            expect(field.important).toEqual(false);
+            break;
+        }
+      });
     });
   });
 });
