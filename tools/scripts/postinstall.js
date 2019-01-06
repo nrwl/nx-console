f76979f2546bf497982db189ac6fbf0d7164ad80
Victor Savkin <vic.savkin@gmail.com>
2019-01-05 21:22:03 -0500

    misc fixes

--- tools/scripts/postinstall.js
+++ tools/scripts/postinstall.js
@@ -1,8 +1,5 @@
 const shell = require('shelljs');
-const cp = require('child_process');
-const path = require('path');
 
 shell.exec('node ./node_modules/vscode/bin/install');
-cp.execSync(`${path.normalize('./node_modules/.bin/electron-rebuild')} -p -t "dev,prod,optional"`, { stdio: [0,1,2]});
 shell.rm('-rf', 'node_modules/cypress/node_modules/@types');
 shell.rm('-rf', 'node_modules/@types/sinon-chai/node_modules/@types');
--- tools/scripts/vscode-yarn.js
+++ tools/scripts/vscode-yarn.js
@@ -1,3 +1,3 @@
 const shell = require('shelljs');
 
-shell.exec('yarn install --production --pure-lockfile --ignore-optional --no-bin-links --non-interactive', {cwd: 'dist\\apps\\vscode'});
+shell.exec('yarn install --production --pure-lockfile --ignore-optional --no-bin-links --non-interactive', {cwd: 'dist/apps/vscode'});
