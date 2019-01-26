fadf44455d98dc51f1a804c5e621d6c689fa8085
Victor Savkin <vic.savkin@gmail.com>
2019-01-26 18:56:23 -0500

    fix(build): add missing deps

--- tools/scripts/vscode-vsce.js
+++ tools/scripts/vscode-vsce.js
@@ -1,6 +1,9 @@
 const cp = require('child_process');
 const path = require('path');
-cp.execSync(`${path.normalize('../../../node_modules/.bin/vsce')} package`, {
-  stdio: [0, 1, 2],
-  cwd: path.normalize('dist/apps/vscode')
-});
+cp.execSync(
+  `${path.normalize('../../../node_modules/.bin/vsce')} package --yarn`,
+  {
+    stdio: [0, 1, 2],
+    cwd: path.normalize('dist/apps/vscode')
+  }
+);
