f8e8e93bece7754de3de584c38fdb9cf6b4e6694
fkolar <frantisek.kolar@hawaia.com>
2019-04-13 23:13:01 +0200

    fix(intellij): Multi instance IDE support
    - Fixes problem while trying to open more than 1 angular based project.
    
    - Fixes `open new project` into the same window
    
    - Fixes race condition while opening toolWindow and server has been started yet
    
    Now it uses one `Angular Console server` for all the IDE instances. Node Server
    is started on the Application level with the first openned angular project and stopped with the
    last one.
    
    We added listener for the `jxBrowser` to  `onFinishLoadingFrame` which
    give us `chrome-error:` message in case server is not started yet (this was the race condition problem)
    which give possibility to reload the URL again until server is up.
    
    Another major change is that we dont register ToolWindow statically inside
    `plugin.xml`, but programatically inside the `NgWorkspaceMonitor` only after
    server has been started and we can access URL.
    
    feat(intellij): Toolbar actions added to the NGConsole ToolWindow
    
    Todo: We need to remove actions from the web app
    
    Also updated version for the Jxbrowser from 6.22 to 6.22.2
    
    feat(intellij): Assign key shortcut to activate/deactive toolWindow
    
    CTRL+SHIFT+C
    
    Then once NgConsole ToolWindow is active you can use
    
    ALT+1 - Workspace
    ALT+2 - Generate
    ALT+3 - Tasks
    ALT+4 - Extensions
    ALT+5 - Connect
    ALT+6 - Settings

--- apps/intellij/src/java/io/nrwl/ide/console/ui/NgConsoleUI.java
+++ apps/intellij/src/java/io/nrwl/ide/console/ui/NgConsoleUI.java
@@ -1,6 +1,9 @@
 package io.nrwl.ide.console.ui;
 
-import com.intellij.openapi.application.Application;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
@@ -8,13 +11,22 @@ import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserContext;
 import com.teamdev.jxbrowser.chromium.BrowserContextParams;
 import com.teamdev.jxbrowser.chromium.BrowserType;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static com.teamdev.jxbrowser.chromium.BrowserPreferences.getDefaultChromiumDir;
 
@@ -25,33 +37,37 @@ import static com.teamdev.jxbrowser.chromium.BrowserPreferences.getDefaultChromi
  * <p>
  * But JxBrowser seems to work pretty well.
  */
-public class NgConsoleUI {
+public class NgConsoleUI implements Disposable {
   private static final Logger LOG = Logger.getInstance(NgConsoleUI.class);
-
-
-  /**
-   * Object representing a Browser build on top of Chromium
-   */
-  private Browser myBrowser;
-
   /**
    * Layout container to place a webview into the CENTER zone
    */
   private final JPanel myPanel = new JPanel(new BorderLayout());
-
+  /**
+   * Object representing a Browser build on top of Chromium
+   */
+  private Browser myBrowser;
   private Map<String, String> myRouteMapping = new HashMap<>();
 
-  private Route myMode = Route.Generate;
+  private Route myRoute = Route.Generate;
 
 
   public NgConsoleUI() {
     myRouteMapping.put(Route.Workspace.name(), "http://localhost:%s/workspace/%s");
+    myRouteMapping.put(Route.Generate.name(), "http://localhost:%s/workspace/%s/generate");
+    myRouteMapping.put(Route.Tasks.name(), "http://localhost:%s/workspace/%s/tasks");
+    myRouteMapping.put(Route.Connect.name(), "http://localhost:%s/workspace/%s/connect");
+    myRouteMapping.put(Route.Extensions.name(), "http://localhost:%s/workspace/%s/extensions");
+    myRouteMapping.put(Route.Settings.name(), "http://localhost:%s/workspace/%s/settings");
   }
 
 
-  public void initWebView(Route mode) {
-    this.myMode = mode;
-    doInitWebView();
+  /**
+   * We are using project name to have unique JxBrowser BrowserContext lock file
+   */
+  public void initWebView(Route mode, String name) {
+    this.myRoute = mode;
+    doInitWebView(name);
   }
 
   public JComponent getContent() {
@@ -63,12 +79,11 @@ public class NgConsoleUI {
    */
   public SimpleToolWindowPanel getToolWindowContent() {
     SimpleToolWindowPanel toolPanel = new SimpleToolWindowPanel(true, true);
-//    ActionManager actionManager = ActionManager.getInstance();
-//    ActionToolbar actionToolbar = actionManager.createActionToolbar(
-//      "toolbar",
-//      (ActionGroup) actionManager.getAction("NGConsole.UI.Toolbar"),
-//      true);
-//    toolPanel.setToolbar(actionToolbar.getComponent());
+    ActionManager actionManager = ActionManager.getInstance();
+    ActionToolbar actionToolbar = actionManager.createActionToolbar("toolbar",
+      (ActionGroup) actionManager.getAction("NGConsole.UI.Toolbar"),
+      true);
+    toolPanel.setToolbar(actionToolbar.getComponent());
     toolPanel.setContent(getContent());
 
     return toolPanel;
@@ -77,42 +92,136 @@ public class NgConsoleUI {
 
   /**
    * Make sure first param is port
-   *
    */
   public void goToUrl(final String... params) {
-    ApplicationManager.getApplication()
-      .invokeLater(() -> {
-        String url = String.format(myRouteMapping.get(myMode.name()), (Object[])params);
+    final String url = String.format(myRouteMapping.get(myRoute.name()), (Object[]) params);
+    LOG.info("Switching to new URL : " + url);
 
-        LOG.info("Switching to new URL : " + url);
-        myBrowser.loadURL(url);
-      });
+    while (!loadAndWait(url)) {
+      try {
+        Thread.sleep(800);
+      } catch (InterruptedException e) {
+      }
+    }
   }
 
 
-  private void doInitWebView() {
+  /**
+   * Make sure first param is port
+   */
+  public void goToUrl(Route route, final String... params) {
+    myRoute = route;
+    final String url = String.format(myRouteMapping.get(myRoute.name()), (Object[]) params);
+    LOG.info("Switching to new URL : " + url);
+
+    while (!loadAndWait(url)) {
+      try {
+        Thread.sleep(800);
+      } catch (InterruptedException e) {
+      }
+    }
+  }
+
+
+  private synchronized boolean loadAndWait(String url) {
+    boolean success;
+
+    CountDownLatch countDown = new CountDownLatch(1);
+    ErrorAwareLoader loader = new ErrorAwareLoader(countDown);
+
+    myBrowser.addLoadListener(loader);
+
+    try {
+      myBrowser.loadURL(url);
+      try {
+        if (!countDown.await((long) 20, TimeUnit.SECONDS)) {
+          throw new RuntimeException(new TimeoutException());
+        }
+      } catch (InterruptedException var7) {
+        Thread.currentThread().interrupt();
+      }
+
+      success = !loader.hasError();
+    } finally {
+      if (myBrowser != null) {
+        myBrowser.removeLoadListener(loader);
+      }
+
+    }
+    return success;
+  }
+
+
+  private void doInitWebView(String name) {
+
     ApplicationManager.getApplication()
-      .invokeLater(() -> {
+      .invokeAndWait(() -> {
+        File dataDir = null;
+        try {
+          dataDir = new File(getDefaultChromiumDir(), "data-" + name);
+          if (dataDir.exists()) {
+            Path path = dataDir.toPath();
+            Files.walk(path).sorted(Comparator.reverseOrder()).map(Path::toFile)
+              .forEach(file -> {
+                if (file != null && file.exists()) {
+                  file.delete();
+                }
+              });
+          }
+        } catch (IOException e) {
+          LOG.error("Problem cleaning up jxBrowser cache: ", e);
+        }
 
-        // Setup unique context Path for the Browser identified by Single IDE APP.
-        Application ideApp = ApplicationManager.getApplication();
-        long timeStamp = ideApp.getStartTime();
-
-        File dataDir = new File(getDefaultChromiumDir(), "data-" + timeStamp);
         BrowserContextParams bcp = new BrowserContextParams(dataDir.getAbsolutePath());
         BrowserContext bc = new BrowserContext(bcp);
 
-        myBrowser = new Browser(BrowserType.getDefault(), bc);
+        myBrowser = new Browser(BrowserType.LIGHTWEIGHT, bc);
         BrowserView view = new BrowserView(myBrowser);
         myPanel.add(view, BorderLayout.CENTER);
       });
   }
 
+  @Override
+  public void dispose() {
+    if (myBrowser != null) {
+      ApplicationManager.getApplication().invokeAndWait(() -> {
+        myBrowser.dispose();
+        myBrowser = null;
+      });
+    }
+  }
 
-  public enum Route {
+
+  public static enum Route {
     NewWorkspace,
+    Workspace,
     Generate,
-    Workspace
+    Tasks,
+    Extensions,
+    Connect,
+    Settings
+  }
+
+  private static class ErrorAwareLoader extends LoadAdapter {
+    private CountDownLatch myLatch;
+    private boolean error = false;
+
+    public ErrorAwareLoader(CountDownLatch latch) {
+      super();
+      myLatch = latch;
+    }
+
+    @Override
+    public void onFinishLoadingFrame(FinishLoadingEvent event) {
+      if (event.isMainFrame()) {
+        error = event.getValidatedURL().contains("chrome-error:");
+        this.myLatch.countDown();
+      }
+    }
+
+    public boolean hasError() {
+      return error;
+    }
   }
 
 
