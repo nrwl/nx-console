76c6746a5b99be2bfb989cae7e76df7d06764dda
Jonathan Cammisuli <jon@cammisuli.ca>
2021-02-13 14:42:21 -0500

    update to nx 10

--- libs/server/jest.config.js
+++ libs/server/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   name: 'server',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/server',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
