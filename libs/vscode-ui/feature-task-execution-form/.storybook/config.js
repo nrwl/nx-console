ca006980b5ff4bc65ad1558cc11487d831cf4cd2
Jonathan Cammisuli <jon@cammisuli.ca>
2021-02-19 15:41:27 -0500

    update storybook

--- libs/vscode-ui/feature-task-execution-form/.storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure, addDecorator } from '@storybook/angular';
-import { withKnobs } from '@storybook/addon-knobs';
-window.vscode = {
-  postMessage: () => {},
-};
-addDecorator(withKnobs);
-configure(require.context('../src/lib', true, /\.stories\.tsx?$/), module);
---
+++ libs/vscode-ui/feature-task-execution-form/.storybook/preview.js
@@ -0,0 +1,4 @@
+import { addDecorator } from '@storybook/angular';
+import { withKnobs } from '@storybook/addon-knobs';
+
+addDecorator(withKnobs);
