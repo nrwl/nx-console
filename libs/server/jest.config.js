4148940cce547315da5550d0ad734b3dfd5439be
Jonathan Cammisuli <jon@cammisuli.ca>
2021-02-19 13:11:22 -0500

    udpate to nx 11.2

--- libs/server/jest.config.js
+++ libs/server/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'server',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/server',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'server',
 };
