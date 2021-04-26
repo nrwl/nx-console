27d69bc8ae60f23a49fed6f3018878b65507d6f5
Sandi Barr <sandi@nrwl.io>
2021-04-26 14:10:11 -0500

    style and formatting to allow for long arguments

--- libs/vscode-ui/components/src/lib/argument-list/argument-list.component.spec.ts
+++ libs/vscode-ui/components/src/lib/argument-list/argument-list.component.spec.ts
@@ -22,4 +22,10 @@ describe('ArgumentListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should format arguments for multiline break-word', () => {
+    component.args = ['--some-arg=withalistofoptions,that-may-not-fit-on-a-single-line'];
+    fixture.detectChanges();
+    expect(component.args).toEqual(['--some-arg=<wbr>withalistofoptions,that-may-not-fit-on-a-single-line']);
+  })
 });
