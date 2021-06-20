64916058d5907b5a6e7d0e347e52f7003523b226
Phillip Barta <barta.phillip@gmail.com>
2021-06-20 17:09:05 +0200

    fix(repo): remove obsolete vscode postinstall

--- tools/scripts/postinstall.js
+++ tools/scripts/postinstall.js
@@ -1,7 +1,4 @@
-const shell = require('shelljs');
 const fs = require('fs');
-
-shell.exec('node ./node_modules/vscode/bin/install');
 
 fs.writeFileSync(
   `./node_modules/source-map-explorer/vendor/webtreemap.css`,
