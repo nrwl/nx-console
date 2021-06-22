e906993a1ec45b877d342760dc59ff203771787e
Jonathan Cammisuli <jon@cammisuli.ca>
2021-06-22 14:19:27 -0400

    fix jest preset to transform @types/vscode

--- libs/server/jest.config.js
+++ libs/server/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/server',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
   displayName: 'server',
 };
