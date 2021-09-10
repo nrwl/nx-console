1efef254272a997e6e35a776e2f5a2c897a344c9
Jonathan Cammisuli <jon@cammisuli.ca>
2021-09-10 15:05:03 -0400

    split chunks and sideeffects false

--- apps/vscode/webpack.prod.config.js
+++ apps/vscode/webpack.prod.config.js
@@ -1,5 +1,14 @@
 module.exports = function transformWebpack(config) {
   // the default node webpack config has minimize: false when optimization is set to true
-  delete config.optimization;
+  config.output.filename = '[name].js';
+  config.optimization = {
+    minimize: true,
+    concatenateModules: true,
+    sideEffects: false,
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
   return config;
 };
