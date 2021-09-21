fc4caeaf3c17c95f1f305d13f6dd062acff5f47c
Jonathan Cammisuli <jon@cammisuli.ca>
2021-09-21 14:31:00 -0400

    feat: use vscode settings for shell execution (#1134)

--- libs/vscode/tasks/src/lib/shell-execution.ts
+++ libs/vscode/tasks/src/lib/shell-execution.ts
@@ -1,12 +1,7 @@
+import { detectPackageManager, getPackageManagerCommand } from '@nrwl/devkit';
 import { ShellExecution } from 'vscode';
-import { platform } from 'os';
-import { execSync } from 'child_process';
 
 export interface ShellConfig {
-  /** Human-readable string which will be used to represent the terminal in the UI. */
-  name: string;
-  program: string;
-  args: string[];
   cwd: string;
   displayCommand: string;
 }
@@ -14,46 +9,13 @@ export interface ShellConfig {
 export function getShellExecutionForConfig(
   config: ShellConfig
 ): ShellExecution {
-  let execution: ShellExecution;
-  if (platform() === 'win32') {
-    execution = getWin32ShellExecution(config);
-  } else {
-    execution = getUnixShellExecution(config);
-  }
+  const packageManager = detectPackageManager(config.cwd);
+  const packageManagerCommand = getPackageManagerCommand(packageManager);
 
-  return execution;
-}
-
-function getWin32ShellExecution(config: ShellConfig): ShellExecution {
-  return new ShellExecution(config.displayCommand, {
-    cwd: config.cwd,
-    executable:
-      'C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
-    shellArgs: [
-      `-Sta -NoLogo -NonInteractive -C "& {${config.program.replace(
-        / /g,
-        '` ' // NOTE: In powershell ` is the escape key.
-      )} ${config.args.join(' ')}}"`,
-    ],
-  });
-}
-
-let bashPath: string;
-function getUnixShellExecution(config: ShellConfig): ShellExecution {
-  if (!bashPath) {
-    try {
-      bashPath = execSync('which bash').toString().trim() || '/bin/bash';
-    } catch {
-      bashPath = '/bin/bash'; // Default to where bash is usually installed.
+  return new ShellExecution(
+    `${packageManagerCommand.exec} ${config.displayCommand}`,
+    {
+      cwd: config.cwd,
     }
-  }
-  return new ShellExecution(config.displayCommand, {
-    cwd: config.cwd,
-    executable: bashPath,
-    shellArgs: [
-      '-l',
-      '-c',
-      `${config.program.replace(/ /g, '\\ ')} ${config.args.join(' ')}`,
-    ],
-  });
+  );
 }
