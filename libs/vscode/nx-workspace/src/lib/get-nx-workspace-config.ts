eb532eacb07aa171dcb3e2c0e43928d4a129e5de
Jonathan Cammisuli <jon@cammisuli.ca>
2021-09-27 10:28:57 -0400

    fix: ensure that projects are properly resolved during async calls

--- libs/vscode/nx-workspace/src/lib/get-nx-workspace-config.ts
+++ libs/vscode/nx-workspace/src/lib/get-nx-workspace-config.ts
@@ -9,7 +9,7 @@ export async function getNxWorkspaceConfig(
   // try and use the workspace version of nx
   try {
     let cachedWorkspaceJson = cacheJson(workspaceJsonPath).json;
-    if (!cachedWorkspaceJson) {
+    if (!cachedWorkspaceJson || hasNxProject(cachedWorkspaceJson)) {
       const workspace = (
         await getNxWorkspacePackageFileUtils()
       ).readWorkspaceConfig({
@@ -22,4 +22,19 @@ export async function getNxWorkspaceConfig(
   } catch (e) {
     return readAndCacheJsonFile(workspaceJsonPath).json;
   }
+}
+
+/**
+ * There are points in time where the async nature of the nx-workspace package is still in being resolved.
+ * This function will check if the projects are strings
+ * @param cachedWorkspaceJson
+ * @returns
+ */
+function hasNxProject(
+  cachedWorkspaceJson?: WorkspaceJsonConfiguration
+): boolean {
+  const projects = cachedWorkspaceJson?.projects ?? {};
+  return Object.entries(projects).some(
+    ([, project]) => typeof project === 'string'
+  );
 }
