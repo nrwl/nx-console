8608bc0d14d49f063a0dd0e095306b8d99af6aec
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-03 10:12:38 -0500

    chore: restore package creation utils :sweat_smile: (#1182)
    This reverts commit bda8885f35b0a77771beba6fad90570251256fee.

--- package-scripts.js
+++ package-scripts.js
@@ -1,4 +1,5 @@
 const nps = require('nps-utils');
+const { join } = require('path');
 
 function affected(affectedCommand) {
   return {
@@ -9,6 +10,7 @@ function affected(affectedCommand) {
 
 module.exports = {
   scripts: {
+    clean: 'shx rm -rf dist/',
     prepare: {
       and: {
         e2e: {
@@ -43,6 +45,12 @@ module.exports = {
         }),
       },
     },
+    package: {
+      vscode: nps.series(
+        `shx rm -rf ${join('dist', 'apps', 'vscode', '**', '*-es5.js')}`,
+        `node ${join('tools', 'scripts', 'vscode-vsce.js')}`
+      ),
+    },
     format: {
       default: 'nx format:write --base=upstream/master',
       and: {
@@ -63,8 +71,11 @@ module.exports = {
       default: 'nx affected:test --all --parallel',
       affected: affected('test'),
     },
-    lint: {
-      default: 'nx run-many --all --parallel --target=lint',
+    'install-dependencies': {
+      vscode: `node ${join('tools', 'scripts', 'vscode-yarn.js')}`,
     },
+    lint: {
+      default: "nx run-many --all --parallel --target=lint"
+    }
   },
 };
---
+++ tools/scripts/vscode-vsce.js
@@ -0,0 +1,11 @@
+const cp = require('child_process');
+const path = require('path');
+cp.execSync(
+  `${path.normalize(
+    '../../../node_modules/.bin/vsce'
+  )} package --yarn --out nx-console.vsix`,
+  {
+    stdio: [0, 1, 2],
+    cwd: path.normalize('dist/apps/vscode'),
+  }
+);
