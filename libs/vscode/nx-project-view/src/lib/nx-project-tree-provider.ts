9b28b377c2c830c646be993de7c895ccd88b2d02
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-21 20:21:02 -0500

    provide better error message

--- libs/vscode/nx-project-view/src/lib/nx-project-tree-provider.ts
+++ libs/vscode/nx-project-view/src/lib/nx-project-tree-provider.ts
@@ -1,4 +1,8 @@
-import { AbstractTreeProvider, clearJsonCache } from '@nx-console/server';
+import {
+  AbstractTreeProvider,
+  clearJsonCache,
+  getOutputChannel,
+} from '@nx-console/server';
 import { WorkspaceConfigurationStore } from '@nx-console/vscode/configuration';
 import { revealNxProject } from '@nx-console/vscode/nx-workspace';
 import { CliTaskProvider } from '@nx-console/vscode/tasks';
@@ -71,9 +75,15 @@ export class NxProjectTreeProvider extends AbstractTreeProvider<NxProjectTreeIte
         nxProject.project
       ];
       if (projectDef) {
-        item.resourceUri = Uri.file(
-          join(this.cliTaskProvider.getWorkspacePath(), projectDef.root)
-        );
+        if (!projectDef.root) {
+          getOutputChannel().appendLine(
+            `Project ${nxProject.project} has no root. This could be because of an error loading the workspace configuration.`
+          );
+        } else {
+          item.resourceUri = Uri.file(
+            join(this.cliTaskProvider.getWorkspacePath(), projectDef.root)
+          );
+        }
       }
       item.contextValue = 'project';
     } else {
