8a2c48f4b31909115b56620f56d92a04d8184e58
Jonathan Cammisuli <jon@cammisuli.ca>
2022-01-07 17:05:59 -0500

    fix: support workspace-generators and workspace-schematics (#1209)

--- libs/server/src/lib/utils/get-generators.ts
+++ libs/server/src/lib/utils/get-generators.ts
@@ -27,14 +27,8 @@ export async function getGenerators(
 
   generatorCollections = [
     ...generatorCollections,
-    ...(await checkAndReadWorkspaceGenerators(
-      basedir,
-      join('tools', 'schematics')
-    )),
-    ...(await checkAndReadWorkspaceGenerators(
-      basedir,
-      join('tools', 'generators')
-    )),
+    ...(await checkAndReadWorkspaceGenerators(basedir, 'schematics')),
+    ...(await checkAndReadWorkspaceGenerators(basedir, 'generators')),
   ];
   return generatorCollections.filter(
     (collection): collection is CollectionInfo => !!collection.data
@@ -43,12 +37,14 @@ export async function getGenerators(
 
 async function checkAndReadWorkspaceGenerators(
   basedir: string,
-  workspaceGeneratorsPath: string
+  workspaceGeneratorType: 'generators' | 'schematics'
 ) {
+  const workspaceGeneratorsPath = join('tools', workspaceGeneratorType);
   if (await directoryExists(join(basedir, workspaceGeneratorsPath))) {
     const collection = await readWorkspaceGeneratorsCollection(
       basedir,
-      workspaceGeneratorsPath
+      workspaceGeneratorsPath,
+      workspaceGeneratorType
     );
     return collection;
   }
@@ -57,10 +53,13 @@ async function checkAndReadWorkspaceGenerators(
 
 async function readWorkspaceGeneratorsCollection(
   basedir: string,
-  workspaceGeneratorsPath: string
+  workspaceGeneratorsPath: string,
+  workspaceGeneratorType: 'generators' | 'schematics'
 ): Promise<CollectionInfo[]> {
   const collectionDir = join(basedir, workspaceGeneratorsPath);
-  const collectionName = 'workspace-generator';
+  const collectionName = `workspace-${
+    workspaceGeneratorType === 'generators' ? 'generator' : 'schematic'
+  }`;
   const collectionPath = join(collectionDir, 'collection.json');
   if (await fileExists(collectionPath)) {
     const collection = await readAndCacheJsonFile(
