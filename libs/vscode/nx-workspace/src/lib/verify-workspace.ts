b1be16e678c4a846eef96e39821ad8f0340ee81b
Jonathan Cammisuli <jon@cammisuli.ca>
2022-06-17 16:09:55 -0400

    fix: cache the results of verify workspace

--- libs/vscode/nx-workspace/src/lib/verify-workspace.ts
+++ libs/vscode/nx-workspace/src/lib/verify-workspace.ts
@@ -3,16 +3,21 @@ import {
   fileExists,
   getOutputChannel,
   getTelemetry,
-  readAndCacheJsonFile,
   toWorkspaceFormat,
 } from '@nx-console/server';
 import { WorkspaceConfigurationStore } from '@nx-console/vscode/configuration';
 import { join } from 'path';
-import { window } from 'vscode';
 import {
-  getNxWorkspaceConfig,
-  NxWorkspaceConfiguration,
-} from './get-nx-workspace-config';
+  firstValueFrom,
+  from,
+  iif,
+  of,
+  ReplaySubject,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { window } from 'vscode';
+import { getNxWorkspaceConfig } from './get-nx-workspace-config';
 
 interface Workspace {
   validWorkspaceJson: boolean;
@@ -22,7 +27,35 @@ interface Workspace {
   workspacePath: string;
 }
 
+const enum Status {
+  not_started,
+  in_progress,
+  cached,
+}
+
+const cachedReplay = new ReplaySubject<Workspace>();
+let status: Status = Status.not_started;
+
 export async function verifyWorkspace(): Promise<Workspace> {
+  return firstValueFrom(
+    iif(
+      () => status === Status.not_started,
+      of({}).pipe(
+        tap(() => {
+          status = Status.in_progress;
+        }),
+        switchMap(() => from(_workspace())),
+        tap((workspace) => {
+          cachedReplay.next(workspace);
+          status = Status.cached;
+        })
+      ),
+      cachedReplay
+    )
+  );
+}
+
+async function _workspace(): Promise<Workspace> {
   const workspacePath = WorkspaceConfigurationStore.instance.get(
     'nxWorkspacePath',
     ''
