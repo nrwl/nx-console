27a248a047452068ac9d2b9909bb74918a404bff
Jonathan Cammisuli <jon@cammisuli.ca>
2022-08-04 09:12:48 -0400

    add package.json + package command

--- apps/nxls/jest.config.ts
+++ apps/nxls/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'lsp',
+  displayName: 'nxls',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/lsp',
+  coverageDirectory: '../../coverage/apps/nxls',
 };
