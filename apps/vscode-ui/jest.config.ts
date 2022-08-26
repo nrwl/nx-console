593d9860de0e1d7ee134f604265e2b5c6a2fe9f8
Jonathan Cammisuli <jon@cammisuli.ca>
2022-08-26 09:27:49 -0400

    chore: update to nx 14.5.x

--- apps/vscode-ui/jest.config.ts
+++ apps/vscode-ui/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default {
   coverageDirectory: '../../coverage/apps/vscode-ui',
 
--- libs/vscode-ui/feature-task-execution-form/jest.config.ts
+++ libs/vscode-ui/feature-task-execution-form/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default {
   displayName: 'vscode-ui-feature-task-execution-form',
 
