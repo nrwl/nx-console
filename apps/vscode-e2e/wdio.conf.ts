61d9d64ef2d6ed821c008ce3b57ad6574175bb5e
Max Kless <maxk@nrwl.io>
2022-10-11 14:05:37 +0200

    feat: record screenshots on e2e failure

--- apps/vscode-e2e/wdio.conf.ts
+++ apps/vscode-e2e/wdio.conf.ts
@@ -305,8 +305,23 @@ export const config: Options.Testrunner = {
    * @param {Boolean} result.passed    true if test has passed, otherwise false
    * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
    */
-  // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-  // },
+  afterTest: function (
+    test,
+    context,
+    { error, result, duration, passed, retries }
+  ) {
+    if (error) {
+      if (!existsSync('./.screenshots')) {
+        mkdirSync('./.screenshots');
+      }
+      const time = new Date();
+      const screenshotTitle =
+        `${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-${time.getUTCHours()}:${time.getUTCMinutes()}:${time.getUTCSeconds()}-${
+          test.parent
+        }-${test.title}`.replace(/[ |/]/, '-');
+      browser.saveScreenshot(`./.screenshots/${screenshotTitle}.png`);
+    }
+  },
 
   /**
    * Hook that gets executed after the suite has ended
