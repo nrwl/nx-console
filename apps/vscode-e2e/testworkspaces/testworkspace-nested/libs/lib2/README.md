d06bcafd32772cb649fcdc0b7494307077272dfd
Max Kless <maxk@nrwl.io>
2022-11-18 12:24:59 +0100

    e2e: add nested testworkspace

---
+++ apps/vscode-e2e/testworkspaces/testworkspace-nested/libs/lib2/README.md
@@ -0,0 +1,7 @@
+# lib2
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test lib2` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ apps/vscode-e2e/testworkspaces/testworkspace-nested/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="app1" />
+      <div />
+    </>
+  );
+}
+
+export default App;
