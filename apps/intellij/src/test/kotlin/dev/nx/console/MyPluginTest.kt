3d028e506c62b8e70e96d8156c5e282d9e942029
Jonathan Cammisuli <jon@cammisuli.ca>
2023-02-09 08:51:21 -0500

    chore(intellij): add ci checks for intellij (#1515)

--- apps/intellij/src/test/kotlin/dev/nx/console/MyPluginTest.kt
+++ apps/intellij/src/test/kotlin/dev/nx/console/MyPluginTest.kt
@@ -1,12 +1,10 @@
 package dev.nx.console
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import dev.nx.console.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -27,12 +25,6 @@ class MyPluginTest : BasePlatformTestCase() {
 
     fun testRename() {
         myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
-    fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
-
-        assertEquals(4, projectService.getRandomNumber())
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
