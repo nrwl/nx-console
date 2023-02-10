f7f04ba670c91ccf1758b5c0b6089d4946c44e55
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-02-10 13:30:34 +0200

    added manually addon-essentials and fixed stories

--- libs/generate-ui/feature-task-execution-form/.storybook/main.js
+++ libs/generate-ui/feature-task-execution-form/.storybook/main.js
@@ -1,11 +1,10 @@
 const rootMain = require('../../../../.storybook/main');
 
-// Use the following syntax to add addons!
-// rootMain.addons.push('');
-rootMain.stories.push(
-  ...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']
-);
-
 module.exports = rootMain;
 
 module.exports.core = { ...module.exports.core, builder: 'webpack5' };
+module.exports.stories = [
+  '../src/lib/**/*.stories.mdx',
+  '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+];
+module.exports.addons = [...rootMain.addons, '@storybook/addon-essentials'];
