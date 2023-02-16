2646bc4bb7afe9e720a04b4adfe4d926e382bbf3
iguissouma <issam.guissouma@laposte.net>
2023-02-16 22:17:58 +0100

    feat(intellij): provide file links for created/updated files

--- apps/intellij/src/main/kotlin/dev/nx/console/generate_ui/run_generator/RunGeneratorManager.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/generate_ui/run_generator/RunGeneratorManager.kt
@@ -69,6 +69,9 @@ class RunGeneratorManager(val project: Project) {
                         val consoleBuilder =
                             TextConsoleBuilderFactory.getInstance().createBuilder(project)
                         val console = consoleBuilder.console
+                        project.basePath?.let {
+                            console.addMessageFilter(NxGeneratorMessageFilter(project, it))
+                        }
                         console.attachToProcess(processHandler)
                         processHandler.startNotify()
 
