eb5e725a5f34d25c7d7d2cbf67b8a1e669c5d76a
iguissouma <issam.guissouma@laposte.net>
2023-02-21 15:34:38 +0100

    fix(intellij): open nx generate ui file on same window

--- apps/intellij/src/main/kotlin/dev/nx/console/generate_ui/editor/NxGenerateUiFile.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/generate_ui/editor/NxGenerateUiFile.kt
@@ -43,6 +43,19 @@ abstract class NxGenerateUiFile(name: String) :
     }
 
     abstract fun createMainComponent(project: Project): JComponent
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NxGenerateUiFile
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = name.hashCode()
 }
 
 class DefaultNxGenerateUiFile(name: String, project: Project) : NxGenerateUiFile(name) {
