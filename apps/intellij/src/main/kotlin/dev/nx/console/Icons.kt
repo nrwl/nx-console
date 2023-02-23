a4c5129e207a885021b818e2164ec422643a2110
Jonathan Cammisuli <jon@cammisuli.ca>
2023-02-23 07:56:47 -0500

    center icons fix intellij icon name

--- apps/intellij/src/main/kotlin/dev/nx/console/Icons.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/Icons.kt
@@ -3,6 +3,6 @@ package dev.nx.console
 import com.intellij.openapi.util.IconLoader
 
 object NxIcons {
-    @JvmField val Action = IconLoader.getIcon("icons/nx-console_dark.svg", javaClass)
-    @JvmField val FileType = IconLoader.getIcon("icons/nx-console_dark.svg", javaClass)
+    @JvmField val Action = IconLoader.getIcon("icons/nx-console.svg", javaClass)
+    @JvmField val FileType = IconLoader.getIcon("icons/nx-console.svg", javaClass)
 }
