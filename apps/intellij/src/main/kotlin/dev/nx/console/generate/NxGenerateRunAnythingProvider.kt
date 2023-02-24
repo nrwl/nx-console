b2b57b65a21072a202c0a99b1dba74775fe293d9
iguissouma <issam.guissouma@laposte.net>
2023-02-24 22:46:46 +0100

    feat(intellij): add nx generate run anything command line provider

---
+++ apps/intellij/src/main/kotlin/dev/nx/console/generate/NxGenerateRunAnythingProvider.kt
@@ -0,0 +1,119 @@
+package dev.nx.console.generate
+
+import com.intellij.execution.executors.DefaultDebugExecutor
+import com.intellij.ide.actions.runAnything.RunAnythingAction
+import com.intellij.ide.actions.runAnything.RunAnythingUtil
+import com.intellij.ide.actions.runAnything.activity.RunAnythingCommandLineProvider
+import com.intellij.ide.actions.runAnything.items.RunAnythingItemBase
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.project.Project
+import dev.nx.console.NxIcons
+import dev.nx.console.generate.run_generator.RunGeneratorManager
+import dev.nx.console.models.NxGenerator
+import dev.nx.console.nxls.server.requests.NxGeneratorOptionsRequestOptions
+import dev.nx.console.services.NxlsService
+import javax.swing.Icon
+import kotlinx.coroutines.runBlocking
+
+class NxGenerateRunAnythingProvider : RunAnythingCommandLineProvider() {
+
+    override fun getIcon(value: String): Icon = NxIcons.Action
+
+    override fun getHelpGroupTitle() = "Nx"
+    override fun execute(dataContext: DataContext, value: String) {
+        super.execute(dataContext, value)
+    }
+
+    override fun getCompletionGroupTitle(): String {
+        return "Nx Generate"
+    }
+
+    override fun getHelpCommandPlaceholder(): String {
+        return "nx generate <generator>"
+    }
+
+    override fun getHelpCommand(): String {
+        return HELP_COMMAND
+    }
+
+    override fun getHelpCommandAliases(): List<String> {
+        return listOf("nx g")
+    }
+
+    override fun getHelpIcon(): Icon = NxIcons.Action
+
+    override fun getMainListItem(dataContext: DataContext, value: String) =
+        RunAnythingItemBase(getCommand(value), NxIcons.Action)
+
+    override fun run(dataContext: DataContext, commandLine: CommandLine): Boolean {
+        val project = RunAnythingUtil.fetchProject(dataContext)
+        val runGeneratorManager = RunGeneratorManager(project)
+        val executor = dataContext.getData(RunAnythingAction.EXECUTOR_KEY)
+        val args = commandLine.parameters.toMutableList()
+        if (args.isEmpty()) {
+            return false
+        }
+        if (executor is DefaultDebugExecutor && "--dryRun" !in args) {
+            args.add("--dryRun")
+        }
+        runGeneratorManager.queueGeneratorToBeRun(args.first(), args.drop(1))
+        return true
+    }
+
+    override fun suggestCompletionVariants(
+        dataContext: DataContext,
+        commandLine: CommandLine
+    ): Sequence<String> {
+        val project = RunAnythingUtil.fetchProject(dataContext)
+        val generators: List<NxGenerator> = runBlocking {
+            NxlsService.getInstance(project).generators()
+        }
+
+        val completeSchematicName = completeSchematicName(commandLine, generators).sorted()
+        val completeOptions =
+            completeOptions(RunAnythingUtil.fetchProject(dataContext), commandLine, generators)
+                .sorted()
+
+        return when {
+            commandLine.toComplete.startsWith("--") -> completeOptions + completeSchematicName
+            commandLine.toComplete.startsWith("-") -> completeOptions + completeSchematicName
+            else -> completeSchematicName + completeOptions
+        }
+    }
+
+    private fun completeSchematicName(
+        commandLine: CommandLine,
+        generators: List<NxGenerator>
+    ): Sequence<String> {
+        return if (generators.any { it.name in commandLine }) emptySequence()
+        else generators.map { it.name }.asSequence()
+    }
+
+    private fun completeOptions(
+        project: Project,
+        commandLine: CommandLine,
+        generators: List<NxGenerator>
+    ): Sequence<String> {
+        val generator = hasGenerator(commandLine, generators) ?: return emptySequence()
+        val generatorOptions = runBlocking {
+            NxlsService.getInstance(project)
+                .generatorOptions(
+                    NxGeneratorOptionsRequestOptions(
+                        collection = generator.data.collection,
+                        name = generator.name,
+                        path = generator.path
+                    )
+                )
+        }
+        return generatorOptions.map { "--${it.name}" }.filterNot { it in commandLine }.asSequence()
+    }
+
+    private fun hasGenerator(
+        commandLine: CommandLine,
+        generators: List<NxGenerator>
+    ): NxGenerator? = generators.find { commandLine.completedParameters.contains(it.name) }
+
+    companion object {
+        const val HELP_COMMAND = "nx generate"
+    }
+}
