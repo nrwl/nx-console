63401a5564ad522062dc1549405a1ee322079d8e
Max Kless <maxk@nrwl.io>
2023-03-01 10:11:05 +0100

    feat: call run anything from action

--- libs/vscode/nx-commands-view/src/lib/nx-commands-provider.ts
+++ libs/vscode/nx-commands-view/src/lib/nx-commands-provider.ts
@@ -73,12 +73,11 @@ export class NxCommandsTreeProvider extends AbstractTreeProvider<NxCommandsTreeI
     const prefixedCommand = command.startsWith('nx ')
       ? command
       : `nx ${command}`;
-    const { workspacePath, workspaceType, isEncapsulatedNx } =
-      await getNxWorkspace();
+    const { workspacePath, isEncapsulatedNx } = await getNxWorkspace();
     const pkgManager = detectPackageManager(workspacePath);
 
     const task = new Task(
-      { type: workspaceType },
+      { type: 'nx' },
       TaskScope.Workspace,
       prefixedCommand,
       pkgManager,
