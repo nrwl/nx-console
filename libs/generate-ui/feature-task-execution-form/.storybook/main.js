15e6f607c7eb042915669deb87e51c7b97e551c9
Jonathan Cammisuli <jon@cammisuli.ca>
2023-03-22 13:46:37 -0400

    fix storybook build

--- libs/generate-ui/feature-task-execution-form/.storybook/main.js
+++ libs/generate-ui/feature-task-execution-form/.storybook/main.js
@@ -1,11 +1,9 @@
 const rootMain = require('../../../../.storybook/main');
-rootMain.addons.push('@storybook/addon-essentials');
-// Use the following syntax to add addons!
-// rootMain.addons.push('');
-rootMain.stories.push(
-  ...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']
-);
 
 module.exports = rootMain;
-
-module.exports.core = { ...module.exports.core, builder: 'webpack5' };
+module.exports.core = { builder: 'webpack5' };
+module.exports.stories = [
+  '../src/lib/**/*.stories.mdx',
+  '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+];
+module.exports.addons = [...rootMain.addons, '@storybook/addon-essentials'];
