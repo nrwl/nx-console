080521b52ccfb63fdd894de39474f9f6e89b9008
Jonathan Cammisuli <jon@cammisuli.ca>
2023-04-05 11:28:15 -0400

    feat(intellij): add console folding

---
+++ apps/intellij/src/main/kotlin/dev/nx/console/ide/ConsoleFolding.kt
@@ -0,0 +1,17 @@
+package dev.nx.console.ide
+
+import com.intellij.execution.ConsoleFolding
+import com.intellij.openapi.project.Project
+
+class ConsoleFolding : ConsoleFolding() {
+    override fun getPlaceholderText(project: Project, lines: MutableList<String>): String? {
+        val nxCommand = lines[0].split(" ").toMutableList()
+        nxCommand[0] = "nx"
+
+        return nxCommand.joinToString(" ")
+    }
+
+    override fun shouldFoldLine(project: Project, line: String): Boolean {
+        return "node_modules/.bin/nx" in line
+    }
+}
