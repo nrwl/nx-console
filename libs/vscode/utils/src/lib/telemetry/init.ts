a74744e70dbc15078e235cf4db4d8cd5a7af8ad4
Jonathan Cammisuli <jon@cammisuli.ca>
2023-04-06 12:20:11 -0400

    fix(vscode): use extension mode to determine production

--- libs/vscode/utils/src/lib/telemetry/init.ts
+++ libs/vscode/utils/src/lib/telemetry/init.ts
@@ -1,3 +1,4 @@
+import { getOutputChannel } from '../output-channel';
 import { Telemetry } from './telemetry';
 
 let telemetry: Telemetry;
@@ -8,6 +9,9 @@ export function getTelemetry() {
 
 // using shared memory here is a shortcut, this should be an api call
 export function initTelemetry(production: boolean) {
+  getOutputChannel().appendLine(
+    `Telemetry: ${production ? 'production' : 'development'}`
+  );
   telemetry = production
     ? Telemetry.withGoogleAnalytics(production)
     : Telemetry.withLogger();
