ba25adff98260137296861035e5df4acdcfe0804
Max Kless <maxk@nrwl.io>
2023-04-27 16:57:05 +0200

    WIP USE JBTABLE

--- apps/intellij/src/main/kotlin/dev/nx/console/settings/options/GeneratorAllowlistSetting.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/settings/options/GeneratorAllowlistSetting.kt
@@ -1,18 +1,18 @@
 package dev.nx.console.settings.options
 
 import com.intellij.openapi.project.Project
-import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.Panel
+import com.intellij.ui.table.JBTable
 import dev.nx.console.settings.NxConsoleSettingBase
 
 class GeneratorAllowlistSetting(val project: Project) : NxConsoleSettingBase<String?> {
 
-    private val inputField: JBTextField = JBTextField().apply { text = "Allowed generators" }
+    private val inputTable: JBTable = JBTable()
     override fun render(panel: Panel) {
-        panel.apply { row { cell(inputField) } }
+        panel.apply { row { cell(inputTable) } }
     }
 
-    override fun getValue(): String? = inputField.text.ifEmpty { null }
+    override fun getValue(): String? = inputTable.getValueAt(0, 0)
 
     override fun setValue(value: String?) {
         if (value == null) return
