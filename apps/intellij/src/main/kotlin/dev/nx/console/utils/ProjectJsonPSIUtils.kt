18e74c3b7ebeacec0b2c8c821072f8cd8c483807
Max Kless <maxk@nrwl.io>
2023-05-16 18:53:55 +0200

    identify configuration nodes

--- apps/intellij/src/main/kotlin/dev/nx/console/utils/ProjectJsonPSIUtils.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/utils/ProjectJsonPSIUtils.kt
@@ -10,38 +10,61 @@ import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
 import com.intellij.psi.util.parentOfType
 
-data class NxTargetDescriptor(val nxProject: String, val nxTarget: String) {}
+data class NxTargetDescriptor(
+    val nxProject: String,
+    val nxTarget: String,
+    val nxTargetConfiguration: String? = null
+) {}
 
 // we should only provide gutters for leaf nodes (see LineMarkerProvider comment)
 // this gets the entire node containing the target for a leaf node
-fun getTargetNodeFromLeafNode(element: PsiElement): JsonProperty? {
+fun getPropertyNodeFromLeafNode(element: PsiElement): JsonProperty? {
     if (element.elementType != JsonElementTypes.DOUBLE_QUOTED_STRING) return null
     val parent = element.parentOfType<JsonStringLiteral>() ?: return null
     return parent.parentOfType()
 }
 
-fun getNxTargetDescriptorFromTargetNode(element: PsiElement): NxTargetDescriptor? {
+fun getNxTargetDescriptorFromNode(element: PsiElement): NxTargetDescriptor? {
     if (element.isValid.not()) {
         return null
     }
 
-    if (!isTargetNodeInsideProjectJson(element)) {
-        return null
-    }
-    val psiProjectJsonFile = element.containingFile as JsonFile
-    psiProjectJsonFile.virtualFile ?: return null
-    val childPropertyLiteral = element.firstChild as? JsonStringLiteral ?: return null
-    val nxTarget = childPropertyLiteral.value
-    val nxProject =
-        (element
-                ?.parentOfType<JsonObject>()
-                ?.parentOfType<JsonObject>()
-                ?.findProperty("name")
-                ?.value as? JsonStringLiteral)
-            ?.value
-            ?: return null
+    if (isTargetNodeInsideProjectJson(element)) {
+        val psiProjectJsonFile = element.containingFile as JsonFile
+        psiProjectJsonFile.virtualFile ?: return null
+        val childPropertyLiteral = element.firstChild as? JsonStringLiteral ?: return null
+        val nxTarget = childPropertyLiteral.value
+        val nxProject =
+            (element
+                    ?.parentOfType<JsonObject>()
+                    ?.parentOfType<JsonObject>()
+                    ?.findProperty("name")
+                    ?.value as? JsonStringLiteral)
+                ?.value
+                ?: return null
 
-    return NxTargetDescriptor(nxProject, nxTarget)
+        return NxTargetDescriptor(nxProject, nxTarget)
+    }
+
+    if (isTargetConfigurationNodeInsideProjectJson(element)) {
+        val childPropertyLiteral = element.firstChild as? JsonStringLiteral ?: return null
+        val nxTargetConfiguration = childPropertyLiteral.value
+        val targetProperty = element.parent.parent.parent.parent as? JsonProperty ?: return null
+        val nxTarget = targetProperty.name
+
+        val nxProject =
+            (targetProperty
+                    ?.parentOfType<JsonObject>()
+                    ?.parentOfType<JsonObject>()
+                    ?.findProperty("name")
+                    ?.value as? JsonStringLiteral)
+                ?.value
+                ?: return null
+
+        return NxTargetDescriptor(nxProject, nxTarget, nxTargetConfiguration)
+    }
+
+    return null
 }
 
 fun isTargetNodeInsideProjectJson(element: PsiElement): Boolean {
@@ -55,6 +78,31 @@ fun isTargetNodeInsideProjectJson(element: PsiElement): Boolean {
     if (isTargetNode(element).not()) {
         return false
     }
+    return true
+}
+
+fun isTargetConfigurationNodeInsideProjectJson(element: PsiElement): Boolean {
+    if (element !is JsonProperty) {
+        return false
+    }
+
+    if (isInsideNxProjectJsonFile(element).not()) {
+        return false
+    }
+
+    val possibleConfigurationsProperty = element.parent.parent as? JsonProperty ?: return false
+
+    if (possibleConfigurationsProperty.name != "configurations") {
+        return false
+    }
+
+    val possibleTargetsProperty =
+        possibleConfigurationsProperty.parent.parent.parent.parent as? JsonProperty ?: return false
+
+    if (possibleTargetsProperty.name != "targets") {
+        return false
+    }
+
     return true
 }
 
