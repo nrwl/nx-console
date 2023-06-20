9a6c5028cd672da444650cca5b45133afed1c0e3
Max Kless <maxk@nrwl.io>
2023-06-20 18:46:58 +0300

    register components in code for cloud webview

--- libs/vscode/nx-cloud-view/src/webview/all-components.ts
+++ libs/vscode/nx-cloud-view/src/webview/all-components.ts
@@ -1,8 +1,14 @@
+import '@nx-console/shared/lit-utils';
+import {
+  allComponents,
+  provideVSCodeDesignSystem,
+} from '@vscode/webview-ui-toolkit';
 import './components/claim-callout';
 import './components/no-cache';
 import './components/run-list';
 import './components/status-labels';
 import './components/steps';
-import '@nx-console/shared/lit-utils';
 import './components/ui/callout';
 import './components/ui/logo';
+
+provideVSCodeDesignSystem().register(allComponents);
