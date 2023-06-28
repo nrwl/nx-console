1a036163fcb7f82b671c24cda7d82b03fb7b33f3
Jonathan Cammisuli <jon@cammisuli.ca>
2023-06-28 06:55:30 -0400

    fix(intellij): do not use `runBlocking` when getting the nx workspace (#1788)

--- apps/intellij/src/main/kotlin/dev/nx/console/utils/NxWorkspace.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/utils/NxWorkspace.kt
@@ -3,6 +3,5 @@ package dev.nx.console.utils
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import dev.nx.console.services.NxlsService
-import kotlinx.coroutines.runBlocking
 
-fun Project.nxWorkspace() = runBlocking { service<NxlsService>().workspace() }
+suspend fun Project.nxWorkspace() = service<NxlsService>().workspace()
