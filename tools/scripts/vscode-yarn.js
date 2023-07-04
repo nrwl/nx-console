d913e9918c71bad01ff15c3e775e52c4c1ef252b
Jonathan Cammisuli <jon@cammisuli.ca>
2023-07-04 07:12:48 -0400

    include all optional @parcel/watcher deps for vscode app

--- tools/scripts/vscode-yarn.js
+++ tools/scripts/vscode-yarn.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { normalize } = require('path');
 
-execSync('npm i', {
+execSync('npm i -f', {
   stdio: 'inherit',
   cwd: normalize('dist/apps/vscode'),
 });
