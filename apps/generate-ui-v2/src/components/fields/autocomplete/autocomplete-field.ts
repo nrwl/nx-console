43f9af477fbdf111b55a399269c3b93b80d8a77d
Max Kless <maxk@nrwl.io>
2023-07-18 12:27:11 +0200

    fix(generate-ui): filter autocomplete options using .includes instead of .startsWith

--- apps/generate-ui-v2/src/components/fields/autocomplete/autocomplete-field.ts
+++ apps/generate-ui-v2/src/components/fields/autocomplete/autocomplete-field.ts
@@ -1,10 +1,11 @@
-import { LitElement, TemplateResult, html } from 'lit';
+import { LitElement, PropertyValueMap, TemplateResult, html } from 'lit';
 import { Field } from '../mixins/field-mixin';
 import { customElement } from 'lit/decorators.js';
 import { FieldWrapper } from '../mixins/field-wrapper-mixin';
 import { map } from 'lit/directives/map.js';
 import { extractItemOptions } from '../../../utils/generator-schema-utils';
 import { spread } from '@open-wc/lit-helpers';
+import { Combobox, ComboboxAutocomplete } from '@microsoft/fast-foundation';
 
 @customElement('autocomplete-field')
 export class AutocompleteField extends FieldWrapper(Field(LitElement)) {
@@ -19,9 +20,10 @@ export class AutocompleteField extends FieldWrapper(Field(LitElement)) {
   private renderVSCode() {
     return html`
       <vscode-combobox
-        autocomplete="both"
+        autocomplete="list"
         position="below"
         @change="${this.handleChange}"
+        @input="${this.handleInput}"
         ${spread(this.ariaAttributes)}
       >
         ${map(
@@ -35,7 +37,7 @@ export class AutocompleteField extends FieldWrapper(Field(LitElement)) {
   private renderIntellij() {
     return html`
       <intellij-combobox
-        autocomplete="both"
+        autocomplete="list"
         position="below"
         @change="${this.handleChange}"
         ${spread(this.ariaAttributes)}
@@ -47,6 +49,45 @@ export class AutocompleteField extends FieldWrapper(Field(LitElement)) {
         )}
       </intellij-combobox>
     `;
+  }
+
+  protected updated(): void {
+    const selector =
+      this.editor === 'vscode' ? 'vscode-combobox' : 'intellij-combobox';
+    const autocompleteNode = this.renderRoot.querySelector(selector);
+    if (!autocompleteNode) {
+      return;
+    }
+    // adapted from https://github.com/microsoft/fast/blob/master/packages/web-components/fast-foundation/src/combobox/combobox.ts
+    autocompleteNode.filterOptions = function () {
+      if (
+        !this.autocomplete ||
+        this.autocomplete === ComboboxAutocomplete.none
+      ) {
+        this.filter = '';
+      }
+
+      const filter = this.filter.toLowerCase();
+
+      this.filteredOptions = this._options.filter((o: any) =>
+        o.text.toLowerCase().includes(this.filter.toLowerCase())
+      );
+
+      if (this.isAutocompleteList) {
+        if (!this.filteredOptions.length && !filter) {
+          this.filteredOptions = this._options;
+        }
+
+        this._options.forEach((o: any) => {
+          o.hidden = !this.filteredOptions.includes(o);
+        });
+      }
+    };
+  }
+
+  private handleInput(e: Event) {
+    const value = e.target as Combobox;
+    console.log(e, value);
   }
 
   private handleChange(e: Event) {
