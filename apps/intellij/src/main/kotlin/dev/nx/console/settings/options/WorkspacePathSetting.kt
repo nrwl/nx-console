45343843a8f00b96bfdadf06669df9e2a7eaa126
Jonathan Cammisuli <jon@cammisuli.ca>
2023-08-14 11:05:55 -0400

    fix(intellij): update deprecated apis

--- apps/intellij/src/main/kotlin/dev/nx/console/settings/options/WorkspacePathSetting.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/settings/options/WorkspacePathSetting.kt
@@ -6,10 +6,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.ui.emptyText
 import com.intellij.ui.components.textFieldWithBrowseButton
+import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.MAX_LINE_LENGTH_WORD_WRAP
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.dsl.builder.RowLayout
-import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import dev.nx.console.services.NxlsService
 import dev.nx.console.settings.NxConsoleSettingBase
 import java.nio.file.Paths
@@ -27,7 +27,7 @@ class WorkspacePathSetting(val project: Project) : NxConsoleSettingBase<String?>
             row {
                     label("Workspace path")
                     cell(inputField)
-                        .horizontalAlign(HorizontalAlign.FILL)
+                        .align(AlignX.FILL)
                         .comment(
                             "Set this if your Nx workspace is not at the root of the currently opened project.",
                             MAX_LINE_LENGTH_WORD_WRAP
