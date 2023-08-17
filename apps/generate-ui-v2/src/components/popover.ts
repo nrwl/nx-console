8b3373c4dcec219eefb4daca175407b5c597d538
Max Kless <maxk@nrwl.io>
2023-08-17 14:04:41 +0200

    plugin to display hints and show them in generate ui

---
+++ apps/generate-ui-v2/src/components/popover.ts
@@ -0,0 +1,44 @@
+import { LitElement, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+
+@customElement('popover-element')
+export class Popover extends LitElement {
+  @property({ type: String })
+  content = '';
+
+  @state()
+  isPopoverVisible = false;
+
+  constructor() {
+    super();
+    this.addEventListener('mouseover', this.showTooltip);
+    this.addEventListener('mouseout', this.hideTooltip);
+  }
+
+  showTooltip() {
+    this.isPopoverVisible = true;
+  }
+
+  hideTooltip() {
+    this.isPopoverVisible = false;
+  }
+
+  render() {
+    return html`
+      <div class="relative inline-block">
+        <slot></slot>
+        <div
+          class="${this.isPopoverVisible
+            ? 'block'
+            : 'hidden'}  tooltip-content border-fieldBorder bg-badgeBackground text-foreground absolute left-0 z-10 w-max max-w-md whitespace-normal border p-1 shadow-md"
+        >
+          ${this.content}
+        </div>
+      </div>
+    `;
+  }
+
+  protected createRenderRoot(): Element | ShadowRoot {
+    return this;
+  }
+}
--- libs/shared/nx-console-plugins/src/lib/internal-plugins/project-name-and-root-processor.ts
+++ libs/shared/nx-console-plugins/src/lib/internal-plugins/project-name-and-root-processor.ts
@@ -12,9 +12,28 @@ export function projectNameAndRootProcessor(
   }
   return {
     ...schema,
-    options: schema.options.filter(
-      (option) => option.name !== 'projectNameAndRootFormat'
-    ),
+    options: schema.options
+      .filter((option) => option.name !== 'projectNameAndRootFormat')
+      .map((option) => {
+        if (option.name === 'name') {
+          return {
+            ...option,
+            'x-hint':
+              "Starting with Nx 16.7, Nx Console will generate files with projectNameAndRootFormat: 'as-provided' set, meaning it generates the project with the exact name and directory provided.",
+            'x-priority': 'important',
+          };
+        }
+        if (option.name === 'directory') {
+          return {
+            ...option,
+            'x-hint':
+              "Starting with Nx 16.7, Nx Console will generate files with projectNameAndRootFormat: 'as-provided' set, meaning it generates the project with the exact name and directory provided.",
+            'x-priority': 'important',
+          };
+        }
+
+        return option;
+      }),
     context: {
       ...schema.context,
       fixedFormValues: {
