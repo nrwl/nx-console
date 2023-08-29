9b031b266359c3011d767010d03a42f78f8a79fc
Max Kless <maxk@nrwl.io>
2023-08-29 11:40:29 +0200

    add debug mode to run configuration

--- apps/intellij/src/main/kotlin/dev/nx/console/run/NxCommandLineState.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/run/NxCommandLineState.kt
@@ -1,23 +1,37 @@
 package dev.nx.console.run
 
-import com.intellij.execution.configurations.CommandLineState
-import com.intellij.execution.process.KillableColoredProcessHandler
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionResult
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.process.ProcessTerminatedListener
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.javascript.nodejs.NodeConsoleAdditionalFilter
-import com.intellij.javascript.nodejs.NodeStackTraceFilter
-import com.intellij.lang.javascript.buildTools.TypeScriptErrorConsoleFilter
+import com.intellij.execution.target.value.TargetValue
+import com.intellij.javascript.debugger.CommandLineDebugConfigurator
+import com.intellij.javascript.nodejs.NodeCommandLineUtil
+import com.intellij.javascript.nodejs.NodeCommandLineUtil.createConsole
+import com.intellij.javascript.nodejs.debug.NodeCommandLineOwner
+import com.intellij.javascript.nodejs.execution.NodeBaseRunProfileState
+import com.intellij.javascript.nodejs.execution.NodeTargetRun
+import com.intellij.javascript.nodejs.execution.NodeTargetRunOptions
+import com.intellij.javascript.nodejs.npm.*
 import com.intellij.openapi.project.Project
 import com.intellij.util.execution.ParametersListUtil
 import dev.nx.console.telemetry.TelemetryService
 import dev.nx.console.utils.*
 
 class NxCommandLineState(
-    environment: ExecutionEnvironment,
+    val environment: ExecutionEnvironment,
     val runConfiguration: NxCommandConfiguration
-) : CommandLineState(environment) {
-    override fun startProcess(): ProcessHandler {
+) : NodeBaseRunProfileState, NodeCommandLineOwner {
+    override fun createExecutionResult(processHandler: ProcessHandler): ExecutionResult {
+        ProcessTerminatedListener.attach(processHandler)
+        val console = createConsole(processHandler, environment.project, true)
+        console.attachToProcess(processHandler)
+        foldCommandLine(console, processHandler)
+        return DefaultExecutionResult(console, processHandler)
+    }
+
+    override fun startProcess(configurator: CommandLineDebugConfigurator?): ProcessHandler {
         val project: Project = environment.project
         val nxRunSettings = runConfiguration.nxRunSettings
         val nxProjects = nxRunSettings.nxProjects.split(",")
@@ -42,27 +56,39 @@ class NxCommandLineState(
                     "${nxProjects.first()}:${nxTargets.first()}${if(nxTargetsConfiguration.isNullOrBlank().not()) ":$nxTargetsConfiguration" else ""}"
                 )
 
-        TelemetryService.getInstance(project)
-            .featureUsed("Nx Run - from context menu/target list/codelens")
+        if (configurator === null) {
+            TelemetryService.getInstance(project)
+                .featureUsed("Nx Run - from context menu/target list/codelens - debug")
+        } else {
+            TelemetryService.getInstance(project)
+                .featureUsed("Nx Run - from context menu/target list/codelens")
+        }
 
-        val commandLine =
-            NxGeneralCommandLine(
+        val targetRun =
+            NodeTargetRun(
+                project.nodeInterpreter,
                 project,
+                configurator,
+                NodeTargetRunOptions.of(true, runConfiguration)
+            )
+        targetRun.envData = nxRunSettings.environmentVariables
+        NodeCommandLineUtil.prependNodeDirToPATH(
+            nxRunSettings.environmentVariables.envs,
+            project.nodeInterpreter
+        )
+
+        targetRun.commandLineBuilder.apply {
+            exePath = TargetValue.fixed(NxExecutable.getExecutablePath(project.nxBasePath))
+
+            addParameters(
                 listOf(
                     *args,
                     *(ParametersListUtil.parseToArray(nxRunSettings.arguments)),
-                ),
-                nxRunSettings.environmentVariables
+                )
             )
+            setWorkingDirectory(project.nxBasePath)
+        }
 
-        val handler = KillableColoredProcessHandler(commandLine)
-        consoleBuilder.console.attachToProcess(handler)
-        addConsoleFilters(
-            NodeStackTraceFilter(project, project.nxBasePath),
-            NodeConsoleAdditionalFilter(project, project.nxBasePath),
-            TypeScriptErrorConsoleFilter(project, project.nxBasePath),
-        )
-        ProcessTerminatedListener.attach(handler) // shows exit code upon termination
-        return handler
+        return targetRun.startProcess()
     }
 }
