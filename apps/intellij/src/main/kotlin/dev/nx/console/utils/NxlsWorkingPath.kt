7d3d95907ed9f27db6a329edf69137e43cfd91d5
Jonathan Cammisuli <jon@cammisuli.ca>
2023-10-17 09:54:57 -0400

    fix: normalize windows path (#1907)

--- apps/intellij/src/main/kotlin/dev/nx/console/utils/NxlsWorkingPath.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/utils/NxlsWorkingPath.kt
@@ -1,10 +1,13 @@
 package dev.nx.console.utils
 
 import com.intellij.execution.wsl.WslPath
+import com.intellij.openapi.util.SystemInfo
 
 fun nxlsWorkingPath(basePath: String) =
     if (WslPath.isWslUncPath(basePath)) {
         WslPath.parseWindowsUncPath(basePath)!!.linuxPath
+    } else if (SystemInfo.isWindows) {
+        basePath.replace("/", "\\")
     } else {
         basePath
     }
