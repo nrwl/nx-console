4bf7f46288490bfcbf818bbfe7cc0c22b1ddba6f
Max Kless <maxk@nrwl.io>
2023-10-19 10:54:37 +0200

    fix(intellij): make sure jcef is supported before opening

--- apps/intellij/src/main/kotlin/dev/nx/console/generate/ui/file/NxGenerateUiFileRenderer.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/generate/ui/file/NxGenerateUiFileRenderer.kt
@@ -1,11 +1,14 @@
 package dev.nx.console.generate.ui.file
 
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.ui.jcef.JBCefApp
 import dev.nx.console.generate.run_generator.RunGeneratorManager
 import dev.nx.console.generate.ui.NxGenerateUiRenderer
 import dev.nx.console.models.NxGenerator
 import dev.nx.console.settings.NxConsoleSettingsProvider
+import dev.nx.console.ui.Notifier
 
 class NxGenerateUiFileRenderer : NxGenerateUiRenderer {
     override fun openGenerateUi(
@@ -13,6 +16,14 @@ class NxGenerateUiFileRenderer : NxGenerateUiRenderer {
         nxGenerator: NxGenerator,
         runGeneratorManager: RunGeneratorManager
     ) {
+        if (!JBCefApp.isSupported()) {
+            Notifier.notifyAnything(
+                project,
+                "Can't open the Generate UI. Your IDE doesn't support JCEF - please use the bundled JetBrains Runtime (JBR) instead of an alternative OpenJDK.",
+                NotificationType.ERROR
+            )
+            return
+        }
         val virtualFile =
             if (NxConsoleSettingsProvider.getInstance().useNewGenerateUIPreview)
                 V2NxGenerateUiFile("Generate", project, runGeneratorManager)
