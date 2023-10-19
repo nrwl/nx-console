5f14b7ec358a857d9071170e96fbaea8640eec14
MaxKless <34165455+MaxKless@users.noreply.github.com>
2023-10-19 16:51:19 +0200

    feat(vscode): show node version loaded by vscode on startup (#1909)

---
+++ libs/vscode/nvm-tip/src/lib/nvm-tip.ts
@@ -0,0 +1,30 @@
+import { GlobalConfigurationStore } from '@nx-console/vscode/configuration';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { ConfigurationTarget, ExtensionContext, window } from 'vscode';
+
+export async function initNvmTip(_: ExtensionContext) {
+  const showTip = GlobalConfigurationStore.instance.get(
+    'showNodeVersionOnStartup'
+  );
+  if (!showTip) {
+    return;
+  }
+  const { stdout } = await promisify(exec)('node -v');
+  const nodeVersion = stdout.trim();
+  window
+    .showInformationMessage(
+      `VSCode loaded Node ${nodeVersion}. [That's wrong?](https://nx.dev/recipes/nx-console/console-troubleshooting#vscode-nvm-issues)`,
+      'OK',
+      "Don't show again"
+    )
+    .then((value) => {
+      if (value != 'OK') {
+        GlobalConfigurationStore.instance.set(
+          'showNodeVersionOnStartup',
+          false,
+          ConfigurationTarget.Global
+        );
+      }
+    });
+}
