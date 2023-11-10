fb6301b532cda5fc043d5e0c35ee4fcda09a4900
Max Kless <maxk@nrwl.io>
2023-11-10 11:58:07 +0000

    fix: distinguish more cases when prefilling

--- libs/shared/nx-console-plugins/src/lib/internal-plugins/prefill-project-and-dir-processor.ts
+++ libs/shared/nx-console-plugins/src/lib/internal-plugins/prefill-project-and-dir-processor.ts
@@ -6,6 +6,9 @@ export const prefillProjectAndDirProcessor: SchemaProcessor = (
   schema: GeneratorSchema,
   workspace: NxWorkspace
 ) => {
+  schema.context = schema.context ?? {};
+  schema.context.prefillValues = schema.context.prefillValues ?? {};
+
   // before nx 17, path/directory options are inconsistent so we don't prefill project & directory simultaneously
   if (workspace.nxVersion.major < 17) {
     if (schema.context?.project) {
@@ -23,14 +26,53 @@ export const prefillProjectAndDirProcessor: SchemaProcessor = (
     }
   }
 
-  // after nx 17, project option won't exist anymore and cwd-ing into directories will be the way to go
+  // after nx 18 the format options will be removed.
+  // That means we should prefill cwd unless there's still a project / projectName option
+  if (workspace.nxVersion.major >= 18) {
+    if (
+      schema.options.find(
+        (o) => o.name === 'project' || o.name === 'projectName'
+      )
+    ) {
+      prefillProject();
+    } else {
+      prefillDirectoryAsCwd();
+    }
+  }
 
-  if (schema.context?.directory) {
-    schema.context.prefillValues = {
-      ...(schema.context.prefillValues ?? {}),
-      cwd: schema.context.directory,
-    };
+  // after nx 17, most generators will have nameAndDirectory or projectNameAndRoot options.
+  // for those, we prefill the cwd
+  if (
+    schema.options.find(
+      (o) =>
+        o.name === 'nameAndDirectoryFormat' ||
+        o.name === 'projectNameAndRootFormat'
+    )
+  ) {
+    prefillDirectoryAsCwd();
+    // if they don't have this option, we prefill the project
+  } else {
+    prefillProject();
   }
 
   return schema;
+
+  function prefillDirectoryAsCwd() {
+    if (schema.context?.directory && schema.context?.prefillValues) {
+      schema.context.prefillValues = {
+        ...schema.context.prefillValues,
+        cwd: schema.context?.directory,
+      };
+    }
+  }
+
+  function prefillProject() {
+    if (schema.context?.project) {
+      schema.context.prefillValues = {
+        ...(schema.context.prefillValues ?? {}),
+        project: schema.context.project,
+        projectName: schema.context.project,
+      };
+    }
+  }
 };
