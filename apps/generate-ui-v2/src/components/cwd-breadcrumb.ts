a6a1a7566449ce8035a3e39536f090c61c285806
Max Kless <maxk@nrwl.io>
2024-01-03 20:04:29 +0100

    jb implementation & fixes

--- apps/generate-ui-v2/src/components/cwd-breadcrumb.ts
+++ apps/generate-ui-v2/src/components/cwd-breadcrumb.ts
@@ -2,6 +2,11 @@ import { LitElement, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { EditorContext } from '../contexts/editor-context';
 import { GeneratorContextContext } from '../contexts/generator-context-context';
+import {
+  intellijFieldColors,
+  intellijFieldPadding,
+  intellijFocusRing,
+} from '../utils/ui-utils';
 
 @customElement('cwd-breadcrumb')
 export class CwdBreadcrumb extends GeneratorContextContext(
@@ -20,10 +25,22 @@ export class CwdBreadcrumb extends GeneratorContextContext(
 
   toggleEdit() {
     this.isEditable = !this.isEditable;
+    if (this.isEditable) {
+      setTimeout(() => {
+        this.renderRoot
+          .querySelector<HTMLInputElement>(
+            this.editor === 'vscode' ? 'vscode-text-field' : 'input'
+          )
+          ?.focus();
+      }, 0);
+    }
   }
 
   confirmEdit() {
-    this.path = this.renderRoot.querySelector('vscode-text-field')?.value || '';
+    this.path =
+      this.renderRoot.querySelector(
+        this.editor === 'vscode' ? 'vscode-text-field' : 'input'
+      )?.value || '';
     this.isEditable = false;
     this.dispatchValue();
   }
@@ -55,18 +72,7 @@ export class CwdBreadcrumb extends GeneratorContextContext(
         </span>
         <span class="mx-2">/</span>
         ${this.isEditable
-          ? html`
-              <vscode-text-field type="text" .value="${this.path}">
-              </vscode-text-field>
-              <icon-element
-                @click="${this.toggleEdit}"
-                icon="close"
-              ></icon-element>
-              <icon-element
-                @click="${this.confirmEdit}"
-                icon="check"
-              ></icon-element>
-            `
+          ? this.renderInlineEdit()
           : html`
               ${pathArray.map(
                 (part, index) => html`
@@ -94,12 +100,47 @@ export class CwdBreadcrumb extends GeneratorContextContext(
     `;
   }
 
+  renderInlineEdit() {
+    if (this.editor === 'vscode') {
+      return html`
+        <vscode-text-field
+          type="text"
+          .value="${this.path}"
+          @keydown="${this.handleInlineEditKeydown}"
+        >
+        </vscode-text-field>
+        <icon-element @click="${this.toggleEdit}" icon="close"></icon-element>
+        <icon-element @click="${this.confirmEdit}" icon="check"></icon-element>
+      `;
+    } else {
+      return html`
+        <input
+          class="${intellijFieldColors} ${intellijFocusRing} ${intellijFieldPadding} rounded"
+          type="text"
+          .value="${this.path}"
+          @keydown="${this.handleInlineEditKeydown}"
+        />
+        <icon-element @click="${this.toggleEdit}" icon="close"></icon-element>
+        <icon-element @click="${this.confirmEdit}" icon="check"></icon-element>
+      `;
+    }
+  }
+
+  private handleInlineEditKeydown(event: KeyboardEvent) {
+    console.log(event);
+    if (event.key === 'Enter') {
+      this.confirmEdit();
+    }
+    if (event.key === 'Escape') {
+      this.toggleEdit();
+    }
+  }
+
   protected updated(_changedProperties: Map<PropertyKey, unknown>): void {
     super.updated(_changedProperties);
 
     if (_changedProperties.has('generatorContext')) {
       const prefillValue = this.generatorContext?.prefillValues?.cwd;
-      console.log('prefillValue', prefillValue);
       if (prefillValue) {
         this.path = prefillValue;
         this.dispatchValue();
