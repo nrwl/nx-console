9529a95b85794c72cf7e66c94f16f3dc22da0e9a
Max Kless <maxk@nrwl.io>
2024-01-09 17:57:52 +0100

    project details json changes

--- libs/vscode/project-details/src/lib/init-vscode-project-details.ts
+++ libs/vscode/project-details/src/lib/init-vscode-project-details.ts
@@ -1,11 +1,120 @@
-import { ExtensionContext, commands, window } from 'vscode';
+import {
+  ExtensionContext,
+  Position,
+  Uri,
+  ViewColumn,
+  commands,
+  window,
+  workspace,
+  Range,
+  ThemeColor,
+  DecorationOptions,
+  TextEditorDecorationType,
+  DecorationRangeBehavior,
+} from 'vscode';
 import { ProjectDetailsManager } from './project-details-manager';
+import { ProjectDetailsProvider } from './project-details-provider';
+import { getProjectByPath } from '@nx-console/vscode/nx-workspace';
+import { showNoProjectAtPathMessage } from '@nx-console/vscode/utils';
+import { parseJsonText } from 'typescript';
 
 export function initVscodeProjectDetails(context: ExtensionContext) {
-  const projectDetailsManager = new ProjectDetailsManager(context);
-  commands.registerCommand('nx.project-details.openToSide', () => {
+  // const projectDetailsManager = new ProjectDetailsManager(context);
+  // commands.registerCommand('nx.project-details.openToSide', () => {
+  //   const uri = window.activeTextEditor?.document.uri;
+  //   if (!uri) return;
+  //   projectDetailsManager.openProjectDetailsToSide(uri);
+  // });
+
+  const projectDetailsProvider = new ProjectDetailsProvider();
+  workspace.registerTextDocumentContentProvider(
+    'project-details',
+    projectDetailsProvider
+  );
+  commands.registerCommand('nx.project-details.openToSide', async () => {
     const uri = window.activeTextEditor?.document.uri;
     if (!uri) return;
-    projectDetailsManager.openProjectDetailsToSide(uri);
+    const project = await getProjectByPath(uri.path);
+    if (!project) {
+      showNoProjectAtPathMessage(uri.path);
+      return;
+    }
+    const doc = await workspace.openTextDocument(
+      Uri.parse(`project-details:${project.name}.project.json`)
+    );
+    await window.showTextDocument(doc, {
+      preview: false,
+      viewColumn: ViewColumn.Beside,
+    });
+
+    // decorateSourceMaps();
   });
 }
+// function highlightTargets() {
+//   const activeEditor = window.activeTextEditor;
+//   if (!activeEditor) return;
+
+//   const document = activeEditor.document;
+//   const text = document.getText();
+
+//   const regex = /"targets"\s*:/g;
+//   let match;
+
+//   const decorationType = window.createTextEditorDecorationType({
+//     borderColor: 'lightblue',
+//     borderStyle: 'solid',
+//     borderWidth: '1px',
+//     borderRadius: '2px',
+//   });
+
+//   const ranges = [];
+
+//   while ((match = regex.exec(text)) !== null) {
+//     const startPos = document.positionAt(match.index);
+//     const endPos = document.positionAt(match.index + match[0].length);
+//     const range = new Range(startPos, endPos);
+//     ranges.push(range);
+//   }
+
+//   activeEditor.setDecorations(decorationType, ranges);
+// }
+
+// const maxSmallIntegerV8 = 2 ** 30;
+// const annotationDecoration: TextEditorDecorationType =
+//   window.createTextEditorDecorationType({
+//     after: {
+//       margin: '0 0 0 3em',
+//       textDecoration: 'none',
+//     },
+//     rangeBehavior: DecorationRangeBehavior.OpenOpen,
+//   });
+
+// function decorateSourceMaps() {
+//   window.onDidChangeTextEditorSelection((e) => {
+//     const line = e.selections[0].active.line;
+//     const decorations = [];
+//     const text = 'helloasdasdasd';
+//     const decorationOptions = {
+//       renderOptions: {
+//         after: {
+//           color: new ThemeColor('input.placeholderForeground'),
+//           contentText: `${'\u00a0'.repeat(10)}${text.replace(/ /g, '\u00a0')}`,
+//           fontWeight: 'normal',
+//           fontStyle: 'normal',
+//           // Pull the decoration out of the document flow if we want to be scrollable
+//           textDecoration: `none;`, //${scrollable ? '' : ' position: absolute;'}`,
+//         },
+//       },
+//       range: new Range(
+//         new Position(line, maxSmallIntegerV8),
+//         new Position(line, maxSmallIntegerV8)
+//       ),
+//     } as DecorationOptions;
+//     decorations.push(decorationOptions);
+//     window.activeTextEditor?.setDecorations(annotationDecoration, decorations);
+//   });
+// }
+
+// function buildLineToPropertyMap(jsonText: string) {
+//   const json = parseJsonText('workspace.json', jsonText);
+// }
---
+++ libs/vscode/project-details/src/lib/project-details-provider.ts
@@ -0,0 +1,47 @@
+import {
+  getNxWorkspaceProjects,
+  getProjectByPath,
+} from '@nx-console/vscode/nx-workspace';
+import { showNoProjectAtPathMessage } from '@nx-console/vscode/utils';
+import {
+  CancellationToken,
+  Event,
+  Position,
+  ProviderResult,
+  TextDocumentContentProvider,
+  Uri,
+  window,
+  Range,
+} from 'vscode';
+
+export class ProjectDetailsProvider implements TextDocumentContentProvider {
+  onDidChange?: Event<Uri> | undefined;
+
+  async provideTextDocumentContent(
+    uri: Uri,
+    token: CancellationToken
+  ): Promise<string | undefined> {
+    const projectName = uri.path.replace('.project.json', '');
+    const project = (await getNxWorkspaceProjects())?.[projectName] as any;
+    delete project.files;
+    delete project['$schema'];
+
+    // Iterate over project and delete empty object or array children
+    const deleteEmptyChildren = (obj: any) => {
+      for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+          deleteEmptyChildren(obj[key]);
+          if (Array.isArray(obj[key]) && obj[key].length === 0) {
+            delete obj[key];
+          } else if (Object.keys(obj[key]).length === 0) {
+            delete obj[key];
+          }
+        }
+      }
+    };
+
+    deleteEmptyChildren(project);
+
+    return JSON.stringify(project, null, 2);
+  }
+}
