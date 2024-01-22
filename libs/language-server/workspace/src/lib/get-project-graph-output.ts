5316de85043c584dc5ae351b361b052eaf4cdb27
Max Kless <maxk@nrwl.io>
2024-01-22 14:49:26 +0100

    fix(nxls): dynamically import cache dir instead of using nx console version

--- libs/language-server/workspace/src/lib/get-project-graph-output.ts
+++ libs/language-server/workspace/src/lib/get-project-graph-output.ts
@@ -1,7 +1,11 @@
-import { cacheDir } from 'nx/src/devkit-exports';
+import { lspLogger } from '@nx-console/language-server/utils';
+import { findNxPackagePath } from '@nx-console/shared/npm';
 import { join, relative, normalize } from 'path';
+import { getNxPackage } from './get-nx-workspace-package';
 
-export function getProjectGraphOutput(workspacePath: string) {
+export async function getProjectGraphOutput(workspacePath: string) {
+  const cacheDir = await getCacheDir(workspacePath);
+
   const directory = join(cacheDir, 'nx-console-project-graph');
   const fullPath = `${directory}/project-graph.html`;
   return {
@@ -9,4 +13,13 @@ export function getProjectGraphOutput(workspacePath: string) {
     relativePath: `./${normalize(relative(workspacePath, fullPath))}`,
     fullPath,
   };
+}
+
+async function getCacheDir(workspacePath: string): Promise<string> {
+  const importPath = await findNxPackagePath(
+    workspacePath,
+    join('src', 'utils', 'cache-directory.js')
+  );
+
+  return ((await getNxPackage(importPath, lspLogger)) as any).cacheDir;
 }
