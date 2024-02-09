3bc39d7f5ec6142c8fef9d50c06da56300525cd4
Jonathan Cammisuli <jon@cammisuli.ca>
2024-02-09 09:45:49 -0500

    feat(vscode): add all codelens to a settings toggle (#2021)

--- libs/vscode/nx-config-decoration/src/lib/init-config-decoration.ts
+++ libs/vscode/nx-config-decoration/src/lib/init-config-decoration.ts
@@ -1,62 +1,9 @@
-import { GlobalConfigurationStore } from '@nx-console/vscode/configuration';
-import {
-  ConfigurationChangeEvent,
-  Disposable,
-  ExtensionContext,
-  languages,
-  workspace,
-} from 'vscode';
-import { WorkspaceCodeLensProvider } from './workspace-codelens-provider';
+import { registerCodeLensProvider } from '@nx-console/vscode/utils';
+import { ExtensionContext } from 'vscode';
 import { updatePluginTargetDecorationsOnEditorChange } from './plugin-target-decorations';
-
-let codeLensProvider: Disposable | null;
+import { WorkspaceCodeLensProvider } from './workspace-codelens-provider';
 
 export function initNxConfigDecoration(context: ExtensionContext) {
-  registerWorkspaceCodeLensProvider(context);
-  watchWorkspaceCodeLensConfigChange(context);
+  registerCodeLensProvider(new WorkspaceCodeLensProvider());
   updatePluginTargetDecorationsOnEditorChange(context);
-}
-
-/**
- * Checks the enableWorkspaceConfigCodeLens setting and registers this as a CodeLensProvider.
- * @param context instance of ExtensionContext from activate
- */
-function registerWorkspaceCodeLensProvider(context: ExtensionContext) {
-  const enableWorkspaceConfigCodeLens = GlobalConfigurationStore.instance.get(
-    'enableWorkspaceConfigCodeLens'
-  );
-  if (enableWorkspaceConfigCodeLens) {
-    codeLensProvider = languages.registerCodeLensProvider(
-      { pattern: '**/{workspace,project}.json' },
-      new WorkspaceCodeLensProvider()
-    );
-    context.subscriptions.push(codeLensProvider);
-  }
-}
-
-/**
- * Watches for settings/configuration changes and enables/disables the CodeLensProvider
- * @param context instance of ExtensionContext from activate
- */
-function watchWorkspaceCodeLensConfigChange(context: ExtensionContext) {
-  context.subscriptions.push(
-    workspace.onDidChangeConfiguration((event: ConfigurationChangeEvent) => {
-      // if the `nxConsole` config changes, check enableWorkspaceConfigCodeLens and register or dispose
-      const affectsNxConsoleConfig = event.affectsConfiguration(
-        GlobalConfigurationStore.configurationSection
-      );
-      if (affectsNxConsoleConfig) {
-        const enableWorkspaceConfigCodeLens =
-          GlobalConfigurationStore.instance.get(
-            'enableWorkspaceConfigCodeLens'
-          );
-        if (enableWorkspaceConfigCodeLens && !codeLensProvider) {
-          registerWorkspaceCodeLensProvider(context);
-        } else if (!enableWorkspaceConfigCodeLens && codeLensProvider) {
-          codeLensProvider.dispose();
-          codeLensProvider = null;
-        }
-      }
-    })
-  );
 }
