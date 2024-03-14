592032a080d4358de970fc317afe5ffcd2aaab5e
Sander.Schutten@athora.nl <Sander.Schutten@athora.nl>
2024-03-14 19:23:50 +0100

    revert unknown changed file

--- apps/intellij/src/main/kotlin/dev/nx/console/utils/FindNxConfigurationFiles.kt
+++ apps/intellij/src/main/kotlin/dev/nx/console/utils/FindNxConfigurationFiles.kt
@@ -4,7 +4,6 @@ import com.intellij.openapi.application.readAction
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VFileProperty
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileVisitor
@@ -21,15 +20,6 @@ suspend fun findNxConfigurationFiles(
                 startDirectory,
                 object : VirtualFileVisitor<Any?>() {
                     override fun visitFile(file: VirtualFile): Boolean {
-                        if (file.`is`(VFileProperty.SYMLINK)) {
-                            val canonicalPath = file.canonicalPath
-                            if (canonicalPath != null) {
-                                if (visitedSymlinks.contains(canonicalPath)) {
-                                    return false
-                                }
-                                visitedSymlinks.add(canonicalPath)
-                            }
-                        }
                         if (
                             !file.isDirectory &&
                                 (file.name == "project.json" ||
