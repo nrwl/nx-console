2cc45b12a66534abf9adc33941129a5a8d2be216
Max Kless <maxk@nrwl.io>
2024-04-08 13:06:30 +0200

    increase jest timeout

--- apps/nxls-e2e/jest.config.ts
+++ apps/nxls-e2e/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  testTimeout: 1000000,
+  testTimeout: 2700000,
 };
