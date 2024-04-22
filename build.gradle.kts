00893b964073f16bc0846860413f13d7d1332c31
Craigory Coppola <craigorycoppola@gmail.com>
2024-04-22 11:57:48 -0400

    chore(repo): enable plugin isolation and bump nx

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,20 @@ tasks {
         gradleVersion = project.findProperty("gradleVersion").toString()
     }
 }
+
+allprojects {
+  apply {
+      plugin("project-report")
+  }
+}
+tasks.register("projectReportAll") {
+    // All project reports of subprojects
+    allprojects.forEach {
+        dependsOn(it.tasks.get("projectReport"))
+    }
+
+    // All projectReportAll of included builds
+    gradle.includedBuilds.forEach {
+        dependsOn(it.task(":projectReportAll"))
+    }
+}
