cc07f0f7c589288a496011d57e3338c296367b2d
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-04-24 17:36:30 +0200

    chore: set up @nx/cypress/plugin (#2102)

--- apps/generate-ui-v2-e2e/cypress.config.ts
+++ apps/generate-ui-v2-e2e/cypress.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname, {
-    bundler: 'vite',
-  }),
+  e2e: {
+    ...nxE2EPreset(__dirname, {
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run generate-ui-v2-e2e:serve',
+      },
+      ciWebServerCommand: 'nx run generate-ui-v2-e2e:serve',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
 });
