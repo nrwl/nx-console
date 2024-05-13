f5dd1ed3500fb40763a2c64bd4a3eb5ea3081aa4
Max Kless <maxk@nrwl.io>
2024-05-13 14:29:52 +0200

    normalize paths in generate ui e2es

--- apps/generate-ui-v2-e2e/setup-lit-dir.mjs
+++ apps/generate-ui-v2-e2e/setup-lit-dir.mjs
@@ -1,10 +1,11 @@
 import fs from 'fs-extra';
+import { normalize } from 'node:path';
 
-fs.copySync('../../dist/apps/generate-ui-v2', './dist/generate-ui-v2');
+fs.copySync(normalize('../../dist/apps/generate-ui-v2'), normalize('./dist/generate-ui-v2'));
 
-fs.createFileSync('./dist/generate-ui-v2/index.html');
+fs.createFileSync(normalize('./dist/generate-ui-v2/index.html'));
 fs.writeFileSync(
-  './dist/generate-ui-v2/index.html',
+  normalize('./dist/generate-ui-v2/index.html'),
   `
     <!DOCTYPE html>
     <html lang="en">
