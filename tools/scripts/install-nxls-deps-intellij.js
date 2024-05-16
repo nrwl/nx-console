5ea2a1765d024c5324b3a504778d3041767a7d4d
Max Kless <maxk@nrwl.io>
2024-05-16 16:31:55 +0200

    formatting & cwd

--- tools/scripts/install-nxls-deps-intellij.js
+++ tools/scripts/install-nxls-deps-intellij.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { normalize } = require('path');
 
-execSync('npm i -f', {
+execSync('npm i -f --verbose', {
   stdio: 'inherit',
   cwd: normalize('dist/apps/intellij/idea-sandbox/plugins/nx-console/nxls'),
 });
