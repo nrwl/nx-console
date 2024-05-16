e03e579a82f7b50ff89ab2cf16d86c322eb6fec5
Max Kless <maxk@nrwl.io>
2024-05-16 16:54:41 +0200

    move to projectjson

--- tools/scripts/install-nxls-deps-intellij.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-const { normalize } = require('path');
-
-execSync('npm i -f --verbose', {
-  stdio: 'inherit',
-  cwd: normalize('dist/apps/intellij/idea-sandbox/plugins/nx-console/nxls'),
-});
