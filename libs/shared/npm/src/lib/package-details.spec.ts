48e3ffb452e9fca596ab1027e9876f8c2916a644
Martin Kapal <flamecze@gmail.com>
2024-05-27 21:22:15 +0200

    test(shared): add unit tests for `importWorkspaceDependency` and `packageDetails`

---
+++ libs/shared/npm/src/lib/package-details.spec.ts
@@ -0,0 +1,48 @@
+import { readAndCacheJsonFile } from '@nx-console/shared-file-system';
+import { packageDetails } from './package-details';
+
+jest.mock('@nx-console/shared-file-system');
+
+describe('packageDetails', () => {
+  it('should return package path, package name and JSON contents', async () => {
+    const readAndCacheJsonFileMock = jest
+      .mocked(readAndCacheJsonFile)
+      .mockResolvedValueOnce({
+        path: 'path',
+        json: {
+          name: 'utils',
+          version: '1.0.0',
+          main: 'dist/index.js',
+          types: 'dist/index.d.ts',
+        },
+      });
+
+    expect(await packageDetails('libs/utils')).toEqual({
+      packagePath: 'libs/utils',
+      packageName: 'utils',
+      packageJson: {
+        name: 'utils',
+        version: '1.0.0',
+        main: 'dist/index.js',
+        types: 'dist/index.d.ts',
+      },
+    });
+    expect(readAndCacheJsonFileMock).toBeCalledWith('libs/utils/package.json');
+  });
+
+  it('should return undefined package name if JSON is empty', async () => {
+    const readAndCacheJsonFileMock = jest
+      .mocked(readAndCacheJsonFile)
+      .mockResolvedValueOnce({
+        path: '',
+        json: {},
+      });
+
+    expect(await packageDetails('')).toEqual({
+      packagePath: '',
+      packageName: undefined,
+      packageJson: {},
+    });
+    expect(readAndCacheJsonFileMock).toBeCalledWith('package.json');
+  });
+});
--- libs/shared/npm/src/lib/workspace-dependencies.spec.ts
+++ libs/shared/npm/src/lib/workspace-dependencies.spec.ts
@@ -1,5 +1,9 @@
+import * as os from 'os';
 import { mocked } from 'jest-mock';
-import { workspaceDependencyPath } from './workspace-dependencies';
+import {
+  importWorkspaceDependency,
+  workspaceDependencyPath,
+} from './workspace-dependencies';
 
 import * as pnpDependencies from './pnp-dependencies';
 jest.mock(
@@ -7,9 +11,9 @@ jest.mock(
   (): Partial<typeof pnpDependencies> => ({
     isWorkspaceInPnp: jest.fn(() => Promise.resolve(false)),
     pnpDependencyPath: jest.fn((workspacePath, dependency) =>
-      Promise.resolve(`.yarn/cache${workspacePath}/${dependency}`)
+      Promise.resolve(`.yarn/cache${workspacePath}/${dependency}`),
     ),
-  })
+  }),
 );
 const mockedPnpDependencies = mocked(pnpDependencies);
 
@@ -23,38 +27,86 @@ jest.mock('@nx-console/shared-file-system', (): Partial<typeof fs> => {
     directoryExists: jest.fn(() => Promise.resolve(true)),
   };
 });
+jest.mock('os', () => {
+  const original = jest.requireActual('os');
+  return {
+    ...original,
+    platform: jest.fn(() => 'darwin'),
+  };
+});
 
-describe('workspace-dependencies path', () => {
-  it('should return a path to a workspace dependency when using node_modules', async () => {
-    const dependencyPath = await workspaceDependencyPath(
-      '/workspace',
-      '@nrwl/nx'
-    );
-    expect(normalize(dependencyPath ?? '')).toEqual(
-      normalize('/workspace/node_modules/@nrwl/nx')
-    );
+describe('workspace-dependencies', () => {
+  describe('workspaceDependencyPath', () => {
+    it('should return a path to a workspace dependency when using node_modules', async () => {
+      const dependencyPath = await workspaceDependencyPath(
+        '/workspace',
+        '@nrwl/nx',
+      );
+      expect(normalize(dependencyPath ?? '')).toEqual(
+        normalize('/workspace/node_modules/@nrwl/nx'),
+      );
+    });
+
+    it('should return a path to a workspace dependency when using yarn pnp', async () => {
+      mockedPnpDependencies.isWorkspaceInPnp.mockImplementationOnce(() =>
+        Promise.resolve(true),
+      );
+      const dependencyPath = await workspaceDependencyPath(
+        '/workspace',
+        '@nrwl/nx',
+      );
+      expect(normalize(dependencyPath ?? '')).toEqual(
+        normalize('.yarn/cache/workspace/@nrwl/nx'),
+      );
+    });
+
+    it('should return a path to a workspace dependency when the dependency name starts with a `.`', async () => {
+      const dependencyPath = await workspaceDependencyPath(
+        '/workspace',
+        './tools/local/executor',
+      );
+      expect(normalize(dependencyPath ?? '')).toEqual(
+        normalize('/workspace/tools/local/executor'),
+      );
+    });
   });
 
-  it('should return a path to a workspace dependency when using yarn pnp', async () => {
-    mockedPnpDependencies.isWorkspaceInPnp.mockImplementationOnce(() =>
-      Promise.resolve(true)
-    );
-    const dependencyPath = await workspaceDependencyPath(
-      '/workspace',
-      '@nrwl/nx'
-    );
-    expect(normalize(dependencyPath ?? '')).toEqual(
-      normalize('.yarn/cache/workspace/@nrwl/nx')
-    );
-  });
+  describe('importWorkspaceDependency', () => {
+    it('should import workspace dependency and log the path', async () => {
+      jest.mock(
+        'node_modules/nx/src/utils.js',
+        () => ({ utilString: 'util-string' }),
+        { virtual: true },
+      );
+      const logMock = jest.fn();
 
-  it('should return a path to a workspace dependency when the dependency name starts with a `.`', async () => {
-    const dependencyPath = await workspaceDependencyPath(
-      '/workspace',
-      './tools/local/executor'
-    );
-    expect(normalize(dependencyPath ?? '')).toEqual(
-      normalize('/workspace/tools/local/executor')
-    );
+      expect(
+        await importWorkspaceDependency('node_modules/nx/src/utils.js', {
+          log: logMock,
+        }),
+      ).toEqual({ utilString: 'util-string' });
+      expect(logMock).toHaveBeenCalledWith(
+        'Using local Nx package at node_modules/nx/src/utils.js',
+      );
+    });
+
+    it('should convert backslashes to forward slashes on Windows', async () => {
+      jest.mock(
+        'node_modules/nx/src/utils.js',
+        () => ({ utilString: 'util-string' }),
+        { virtual: true },
+      );
+      jest.mocked(os.platform).mockReturnValueOnce('win32');
+      const logMock = jest.fn();
+
+      expect(
+        await importWorkspaceDependency('node_modules\\nx\\src\\utils.js', {
+          log: logMock,
+        }),
+      ).toEqual({ utilString: 'util-string' });
+      expect(logMock).toHaveBeenCalledWith(
+        'Using local Nx package at node_modules/nx/src/utils.js',
+      );
+    });
   });
 });
