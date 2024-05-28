5ffc97eb4401b54a3b53d3a163114727556bbe36
Martin Kapal <flamecze@gmail.com>
2024-05-28 20:31:38 +0200

    fix: `npmDependencies` returns dependencies from `.nx/installation/node_modules` if `node_modules` exists but is not a directory

---
+++ libs/shared/npm/src/lib/npm-dependencies.spec.ts
@@ -0,0 +1,83 @@
+import { npmDependencies } from './npm-dependencies';
+import { directoryExists, readDirectory } from '@nx-console/shared/file-system';
+
+jest.mock('@nx-console/shared/file-system');
+
+describe('npmDependencies', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should return no dependencies if `node_modules` and `.nx/installation/node_modules` do not exist', async () => {
+    jest.mocked(directoryExists).mockResolvedValue(false);
+
+    expect(await npmDependencies('workspace')).toEqual([]);
+  });
+
+  it('should return dependencies from `node_modules` if `node_modules` exists', async () => {
+    jest
+      .mocked(directoryExists)
+      .mockImplementation((filePath) =>
+        Promise.resolve(
+          [
+            'workspace/node_modules',
+            'workspace/node_modules/package-foo',
+            'workspace/node_modules/package-bar',
+          ].includes(filePath)
+        )
+      );
+    jest
+      .mocked(readDirectory)
+      .mockResolvedValue(['package-foo', 'package-bar']);
+
+    expect(await npmDependencies('workspace')).toEqual([
+      'workspace/node_modules/package-foo',
+      'workspace/node_modules/package-bar',
+    ]);
+  });
+
+  it('should return dependencies from `.nx/installation/node_modules` if only `.nx/installation/node_modules` exists', async () => {
+    jest
+      .mocked(directoryExists)
+      .mockImplementation((filePath) =>
+        Promise.resolve(
+          [
+            'workspace/.nx/installation/node_modules',
+            'workspace/.nx/installation/node_modules/package-foo',
+            'workspace/.nx/installation/node_modules/package-bar',
+          ].includes(filePath)
+        )
+      );
+    jest
+      .mocked(readDirectory)
+      .mockResolvedValue(['package-foo', 'package-bar']);
+
+    expect(await npmDependencies('workspace')).toEqual([
+      'workspace/.nx/installation/node_modules/package-foo',
+      'workspace/.nx/installation/node_modules/package-bar',
+    ]);
+  });
+
+  it('should return packages from `node_modules` if `node_modules` and `.nx/installation/node_modules` exist', async () => {
+    jest
+      .mocked(directoryExists)
+      .mockImplementation((filePath) =>
+        Promise.resolve(
+          [
+            'workspace/node_modules',
+            'workspace/node_modules/package-foo',
+            'workspace/node_modules/package-bar',
+            'workspace/.nx/installation/node_modules',
+          ].includes(filePath)
+        )
+      );
+    jest
+      .mocked(readDirectory)
+      .mockResolvedValue(['package-foo', 'package-bar']);
+
+    expect(await npmDependencies('workspace')).toEqual([
+      'workspace/node_modules/package-foo',
+      'workspace/node_modules/package-bar',
+    ]);
+  });
+});
