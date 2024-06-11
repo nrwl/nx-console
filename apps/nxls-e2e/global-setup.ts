49389953effd2cc1f3a091fdcfcba9f2b04234a5
Max Kless <maxk@nrwl.io>
2024-06-11 11:44:20 +0200

    try again after cloud changes

--- apps/nxls-e2e/global-setup.ts
+++ apps/nxls-e2e/global-setup.ts
@@ -1,9 +1,11 @@
 import { Config } from '@jest/types';
 import { execSync } from 'child_process';
+import { join } from 'path';
 
 export default async function (globalConfig: Config.ConfigGlobals[]) {
-  execSync('npx nx run nxls:build --skip-nx-cache', {
+  execSync('npx nx run nxls:build --verbose', {
     stdio: 'inherit',
     windowsHide: true,
+    cwd: join(__dirname, '..', '..'),
   });
 }
