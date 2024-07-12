c70e8cdcb15a500f538ea487d56f5622cb7d72e4
MaxKless <34165455+MaxKless@users.noreply.github.com>
2024-07-12 16:21:32 +0200

    chore(repo): add projectReportAll gradle task to get rid of error (#2211)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,15 @@ allprojects {
       plugin("project-report")
   }
 }
+
+tasks.register("projectReportAll") {
+    // All project reports of subprojects
+    allprojects.forEach {
+        dependsOn(it.tasks.get("projectReport"))
+    }
+
+    // All projectReportAll of included builds
+    gradle.includedBuilds.forEach {
+        dependsOn(it.task(":projectReportAll"))
+    }
+}
