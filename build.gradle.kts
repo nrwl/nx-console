b2d3520c81db1d4de6b474b7ea3d4aecfd7cca58
Emily Xiong <xiongemi@gmail.com>
2024-09-17 22:34:39 -0400

    add --skip-nx-cache for format

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,19 @@ group = "dev.nx.console"
 layout.buildDirectory = File("dist")
 
 tasks { wrapper { gradleVersion = project.findProperty("gradleVersion").toString() } }
-
-allprojects { apply { plugin("project-report") } }
-
-tasks.register("projectReportAll") {
+allprojects {
+  apply {
+      plugin("project-report")
+  }
+}
+tasks.register("projectReportAll") {
     // All project reports of subprojects
-    allprojects.forEach { dependsOn(it.tasks.get("projectReport")) }
+    allprojects.forEach {
+        dependsOn(it.tasks.get("projectReport"))
+    }
 
     // All projectReportAll of included builds
-    gradle.includedBuilds.forEach { dependsOn(it.task(":projectReportAll")) }
+    gradle.includedBuilds.forEach {
+        dependsOn(it.task(":projectReportAll"))
+    }
 }
