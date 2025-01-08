1ca3598d06ed32d2a97ac84ab8aeb8817fb3c3ce
Max Kless <maxk@nrwl.io>
2025-01-08 14:47:40 +0100

    copy single dep over directly instead of running npm i

--- tools/scripts/copy-to-vscode.js
+++ tools/scripts/copy-to-vscode.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const { normalize } = require('path');
 
 // copy dependency outputs
 const nxlsDest = './dist/apps/vscode/nxls';
@@ -15,3 +16,29 @@ fs.copySync(
 
 // copy package.json
 fs.copySync('./apps/vscode/package.json', './dist/apps/vscode/package.json');
+
+// copy required dependencies
+//  we don't need the entire @vscode-elements/elements package, just the bundled.js file
+const destFolder = normalize(
+  './dist/apps/vscode/node_modules/@vscode-elements/elements/dist'
+);
+if (!fs.existsSync(destFolder)) {
+  fs.mkdirSync(destFolder, { recursive: true });
+}
+fs.copyFileSync(
+  normalize('./node_modules/@vscode-elements/elements/dist/bundled.js'),
+  normalize(`${destFolder}/bundled.js`)
+);
+
+// copy the typescript plugin which is required at runtime
+const typescriptPluginDestFolder = normalize(
+  './dist/apps/vscode/node_modules/@monodon/typescript-nx-imports-plugin'
+);
+if (!fs.existsSync(typescriptPluginDestFolder)) {
+  fs.mkdirSync(typescriptPluginDestFolder, { recursive: true });
+}
+fs.cpSync(
+  normalize('./node_modules/@monodon/typescript-nx-imports-plugin'),
+  normalize(`${typescriptPluginDestFolder}`),
+  { recursive: true }
+);
