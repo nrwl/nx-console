37b951b425ea4aa12ffe8e17a8b98ec199817075
Max Kless <maxk@nrwl.io>
2025-01-10 09:51:48 +0100

    dummy migrate ui webview

--- libs/vscode/migrate-sidebar-webview/src/main.ts
+++ libs/vscode/migrate-sidebar-webview/src/main.ts
@@ -2,6 +2,7 @@ import { html, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import type { MigrateViewData } from '@nx-console/vscode-migrate';
+import type { WebviewApi } from 'vscode-webview';
 
 @customElement('root-element')
 export class Root extends LitElement {
@@ -11,11 +12,19 @@ export class Root extends LitElement {
   @property({ type: Object })
   protected migrateViewData: MigrateViewData | undefined;
 
+  private vscodeApi: WebviewApi<undefined> = acquireVsCodeApi();
+
   override render(): TemplateResult {
     if (this.state === 'update-available') {
       return html`
         <p>A newer version of Nx is available to migrate to :)</p>
-        <vscode-button>Update</vscode-button>
+        <vscode-button
+          @click="${() => {
+            console.log('clicked');
+            this.vscodeApi.postMessage({ type: 'open' });
+          }}"
+          >Update</vscode-button
+        >
       `;
     } else if (this.state === 'in-progress') {
       return html`
