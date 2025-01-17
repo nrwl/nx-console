de5fcb5a8fba52d8b90822a747a5e52550906612
MaxKless <34165455+MaxKless@users.noreply.github.com>
2025-01-17 10:16:49 +0100

    fix(nxls): install deps only in nested folder (#2384)

--- tools/scripts/copy-nxls-package-json.js
+++ tools/scripts/copy-nxls-package-json.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const { execSync } = require('child_process');
 fs.copySync('./apps/nxls/package.json', './dist/apps/nxls/package.json');
 
-execSync(`npm i -f`, {
+execSync(`npm i -f --install-strategy nested`, {
   stdio: 'inherit',
   cwd: './dist/apps/nxls',
 });
