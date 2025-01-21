395887922ee3d54de9a94464bd4298ac5d1b7626
Max Kless <maxk@nrwl.io>
2025-01-21 13:14:56 -0600

    run migration via function imported from nx

--- libs/vscode/migrate/src/lib/commands/run-migration.ts
+++ libs/vscode/migrate/src/lib/commands/run-migration.ts
@@ -4,84 +4,75 @@ import { getOutputChannel } from '@nx-console/vscode-output-channels';
 import { execSync } from 'child_process';
 import { existsSync, mkdirSync, writeFileSync, rmSync } from 'fs';
 import type { GeneratedMigrationDetails } from 'nx/src/config/misc-interfaces';
-import { join, relative } from 'path';
+import { join, relative, resolve } from 'path';
 import { window, ProgressLocation } from 'vscode';
 import { modifyMigrationsJsonMetadata } from './utils';
 import { getPackageManagerCommand } from '@nx-console/shared-utils';
+import { nxCliPath } from 'nx/src/command-line/migrate/migrate';
 
 export async function runSingleMigration(
   migration: GeneratedMigrationDetails,
   configuration: { createCommits: boolean }
 ) {
-  const nxWorkspacePath = getNxWorkspacePath();
-
-  const cacheDirectoryModule = await importNxPackagePath<
-    typeof import('nx/src/utils/cache-directory')
-  >(nxWorkspacePath, 'src/utils/cache-directory');
-
-  const tmpFolder =
-    cacheDirectoryModule.workspaceDataDirectory ??
-    cacheDirectoryModule.cacheDir;
-
-  const tmpMigrationsJsonPath = join(
-    tmpFolder,
-    `migrations~${new Date().getTime()}.json`
-  );
-
-  if (!existsSync(tmpFolder)) {
-    mkdirSync(tmpFolder, { recursive: true });
-  }
-  writeFileSync(
-    tmpMigrationsJsonPath,
-    JSON.stringify({ migrations: [migration] }, null, 2)
-  );
-  const relativePath = relative(getNxWorkspacePath(), tmpMigrationsJsonPath);
-  const pm = await getPackageManagerCommand(getNxWorkspacePath());
+  const workspacePath = getNxWorkspacePath();
 
   window.withProgress(
     {
       location: ProgressLocation.Notification,
       title: `Running ${migration.name}`,
-      cancellable: false,
     },
     async () => {
       try {
-        const result = execSync(
-          `${pm.exec} nx migrate --runMigrations=${relativePath} ${
-            configuration.createCommits ? '--createCommits' : ''
-          }`,
-          {
-            cwd: getNxWorkspacePath(),
-          }
+        // TODO: remove this once actual version is released
+        //       the nx implementation ignores custom registries here
+        process.env['NX_MIGRATE_CLI_VERSION'] = '21.0.14-local';
+        const cliPath = nxCliPath(workspacePath);
+        const updatedMigrateLocation = resolve(
+          cliPath,
+          '..',
+          '..',
+          'nx',
+          'src',
+          'command-line',
+          'migrate',
+          'migrate.js'
         );
 
-        if (result.includes(`Failed to run ${migration.name}`)) {
-          modifyMigrationsJsonMetadata(
-            addFailedMigration(migration.name, result.toString())
-          );
-        } else {
-          modifyMigrationsJsonMetadata(
-            addSuccessfulMigration(migration.name, result.toString())
-          );
-        }
+        const updatedMigrateModule: typeof import('nx/src/command-line/migrate/migrate') =
+          await import(updatedMigrateLocation);
+
+        const fileChanges = await updatedMigrateModule.runNxOrAngularMigration(
+          workspacePath,
+          migration,
+          false,
+          configuration.createCommits,
+          'chore: [nx migration] '
+        );
+
+        modifyMigrationsJsonMetadata(
+          addSuccessfulMigration(
+            migration.name,
+            fileChanges.map((change) => change.path)
+          )
+        );
       } catch (e) {
-        modifyMigrationsJsonMetadata(addFailedMigration(migration.name, e));
-        getOutputChannel().appendLine(e);
+        console.error(e);
+        modifyMigrationsJsonMetadata(
+          addFailedMigration(migration.name, e.message)
+        );
       }
     }
   );
-
-  rmSync(tmpMigrationsJsonPath);
 }
 
-function addSuccessfulMigration(name: string, result: string) {
+function addSuccessfulMigration(name: string, fileChanges: string[]) {
   return (migrationsJsonMetadata: any) => {
     if (!migrationsJsonMetadata.successfulMigrations) {
       migrationsJsonMetadata.successfulMigrations = [];
     }
     migrationsJsonMetadata.successfulMigrations.push({
       name: name,
-      changes: result.includes('No changes were made') ? [] : ['dummy'],
+      changes: fileChanges,
     });
     return migrationsJsonMetadata;
   };
