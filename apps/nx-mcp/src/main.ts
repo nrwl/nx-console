50618e02a0740075ceee908278a82d3c154d4fc0
Max Kless <maxk@nrwl.io>
2025-02-20 17:23:34 +0100

    return proper formatted data

--- apps/nx-mcp/src/main.ts
+++ apps/nx-mcp/src/main.ts
@@ -1,25 +1,56 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import { fileURLToPath } from 'url';
-import { z } from 'zod';
+import { checkIsNxWorkspace } from '@nx-console/shared-npm';
+import { nxWorkspace } from '@nx-console/shared-nx-workspace-info';
+import { getProjectGraphPrompt } from '@nx-console/shared-prompts';
+import { getMcpLogger } from './mcp-logger';
+
+const nxWorkspacePath = process.argv[2];
+if (!nxWorkspacePath) {
+  console.error('Please provide a workspace root as the first argument');
+  process.exit(1);
+}
 
 const server = new McpServer({
   name: 'Nx MCP',
   version: '0.0.1',
 });
+server.server.registerCapabilities({
+  logging: {},
+});
+const logger = getMcpLogger(server);
 
 server.tool(
-  'project-graph',
-  'Returns a readable representation of the nx project graph and global nx configuration. Use it to answer questions about the nx workspace and architecture',
+  'nx-project-graph',
+  'Returns a readable representation of the nx project graph. Use it to answer questions about the nx workspace and architecture',
   async () => {
-    const roots = await server.server.listRoots();
-    const nxWorkspacePath = fileURLToPath(roots.roots[0].uri);
+    try {
+      if (!(await checkIsNxWorkspace(nxWorkspacePath))) {
+        return {
+          isError: true,
+          content: [
+            {
+              type: 'text',
+              text: 'Error: The provided root is not a valid nx workspace.',
+            },
+          ],
+        };
+      }
 
-    return {
-      content: [
-        { type: 'text', text: String(roots.roots.map((root) => root.uri)) },
-      ],
-    };
+      const workspace = await nxWorkspace(nxWorkspacePath, logger);
+      return {
+        content: [
+          {
+            type: 'text',
+            text: getProjectGraphPrompt(workspace.projectGraph),
+          },
+        ],
+      };
+    } catch (e) {
+      return {
+        content: [{ type: 'text', text: String(e) }],
+      };
+    }
   },
 );
 
---
+++ libs/shared/prompts/src/lib/project-graph-prompt.ts
@@ -0,0 +1,49 @@
+import type { ProjectGraph } from 'nx/src/devkit-exports';
+
+export function getProjectGraphPrompt(projectGraph: ProjectGraph): string {
+  return `
+The following is a representation of the Nx workspace. It includes all
+projects in the monorepo. The projects are separated by <></> tags including the project name.
+Each project contains:
+- its dependencies (projects that this depends on), marked by "deps: [...]".
+- its available targets, marked by "targets: [...]". Targets are tasks that the user can run for each project.
+- its type (libary, app, or e2e tests), marked by "type: [...]".
+- its source file location, marked by "root: [...]".
+- some metadata like tags, owners or technologies used.
+
+This data is very important. Use it to analyze the workspace and provide relevant answers to the user. 
+The user cannot see this data, so don't reference it directly. It is read-only, so don't suggest modifications to it.
+
+${getRobotReadableProjectGraph(projectGraph)}
+`.trim();
+}
+
+function getRobotReadableProjectGraph(projectGraph: ProjectGraph): string {
+  let serializedGraph = '';
+  Object.entries(projectGraph.nodes).forEach(([name, node]) => {
+    let nodeString = `<${name}>`;
+    nodeString += `deps:[${projectGraph.dependencies[name]
+      .filter((dep) => !dep.target.startsWith('npm:'))
+      .map((dep) => dep.target)
+      .join(', ')}]`;
+    nodeString += `targets:[${Object.keys(node.data.targets ?? {}).join(', ')}]`;
+    nodeString += `type:[${node.type}]`;
+    nodeString += `root:[${node.data.root}]`;
+    if (node.data.metadata?.technologies) {
+      nodeString += `technologies:[${node.data.metadata.technologies.join(
+        ', ',
+      )}]`;
+    }
+    if (node.data.metadata?.owners) {
+      nodeString += `owners:[${Object.keys(node.data.metadata.owners).join(
+        ', ',
+      )}]`;
+    }
+    if (node.data.tags) {
+      nodeString += `tags:[${node.data.tags.join(', ')}]`;
+    }
+    nodeString += `<${name}/>\n`;
+    serializedGraph += nodeString;
+  });
+  return serializedGraph;
+}
