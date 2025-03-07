f315ec60eda5be5e4170bf9b557809656bc95707
Max Kless <maxk@nrwl.io>
2025-03-07 12:24:15 +0100

    chore(repo): fix intelllij publishing, update versions, release new nx-mcp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,17 @@ group = "dev.nx.console"
 
 layout.buildDirectory = File("dist")
 
-allprojects {
-  apply {
-    plugin("project-report")
-  }
-}
+allprojects { apply { plugin("project-report") } }
 
 tasks.register("projectReportAll") {
-  // All project reports of subprojects
-  allprojects.forEach {
-    dependsOn(it.tasks.get("projectReport"))
-  }
+    // All project reports of subprojects
+    allprojects.forEach { dependsOn(it.tasks.get("projectReport")) }
 
-  // All projectReportAll of included builds
-  gradle.includedBuilds.forEach {
-    dependsOn(it.task(":projectReportAll"))
-  }
+    // All projectReportAll of included builds
+    gradle.includedBuilds.forEach { dependsOn(it.task(":projectReportAll")) }
+}
+
+tasks.register<DefaultTask>("publish") {
+    group = "publish"
+    description = "Placeholder task to workaround the semantic-release plugin"
 }
