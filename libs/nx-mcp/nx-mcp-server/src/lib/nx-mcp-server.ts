9bac427d665ab3739786de9a7391db73dd3312cf
Max Kless <maxk@nrwl.io>
2025-04-01 10:29:19 +0200

    read examples.md files

--- libs/nx-mcp/nx-mcp-server/src/lib/nx-mcp-server.ts
+++ libs/nx-mcp/nx-mcp-server/src/lib/nx-mcp-server.ts
@@ -135,7 +135,7 @@ export class NxMcpServerWrapper {
 
         let nxVersion: NxVersion | undefined = undefined;
         let nxWorkspace: NxWorkspace | undefined = undefined;
-        let workspacePath: string | undefined = this._nxWorkspacePath;
+        const workspacePath: string | undefined = this._nxWorkspacePath;
 
         if (this._nxWorkspacePath) {
           nxWorkspace = await this.nxWorkspaceInfoProvider.nxWorkspace(
@@ -375,6 +375,18 @@ export class NxMcpServerWrapper {
           generators,
         );
 
+        let examples = '';
+        try {
+          const examplesPath = path.join(
+            generators.find((g) => g.name === generatorName)?.schemaPath ?? '',
+            '..',
+            'examples.md',
+          );
+          examples = await readFile(examplesPath, 'utf-8');
+        } catch (e) {
+          examples = 'No examples available';
+        }
+
         return {
           content: [
             {
@@ -397,6 +409,10 @@ Found generator schema for ${generatorName}: ${JSON.stringify(
 This approach provides better clarity about where new code will be generated
 and follows the Nx workspace convention for project organization.
             `,
+            },
+            {
+              type: 'text',
+              text: 'Examples: \n' + examples,
             },
           ],
         };
