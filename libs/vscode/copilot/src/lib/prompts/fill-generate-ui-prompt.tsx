d4d56fed42e7c9dd1bf8c4152fbf11ae711a0800
Max Kless <maxk@nrwl.io>
2025-04-01 16:31:13 +0200

    further work on generate button

--- libs/vscode/copilot/src/lib/prompts/fill-generate-ui-prompt.tsx
+++ libs/vscode/copilot/src/lib/prompts/fill-generate-ui-prompt.tsx
@@ -1,3 +1,4 @@
+import { FormValues } from '@nx-console/shared-generate-ui-types';
 import {
   BasePromptElementProps,
   PromptElement,
@@ -7,10 +8,12 @@ import {
 } from '@vscode/prompt-tsx';
 import { ChatResponsePart } from '@vscode/prompt-tsx/dist/base/vscodeTypes';
 import { Progress, CancellationToken } from 'vscode';
-
-export interface FillGenerateUIPromptProps extends BasePromptElementProps {
-  formValues: Record<string, any>;
+import { NxCopilotPromptProps } from './prompt';
+import { DocsPagesPrompt } from './docs-pages-prompt';
+export interface FillGenerateUIPromptProps extends NxCopilotPromptProps {
+  formValues: FormValues;
   generatorName: string;
+  generatorSchema: any;
 }
 
 export class FillGenerateUIPrompt extends PromptElement<FillGenerateUIPromptProps> {
@@ -21,16 +24,34 @@ export class FillGenerateUIPrompt extends PromptElement<FillGenerateUIPromptProp
     token?: CancellationToken,
   ): Promise<PromptPiece | undefined> | PromptPiece | undefined {
     return (
-      <UserMessage priority={100}>
-        You are an AI assistant specialized in Nx workspaces and monorepo
-        development. You provide precise, technical guidance for developers
-        working with Nx tools, patterns, and best practices. Use the provided
-        tools available to you to answer the users query. YOU HAVE ONE JOB AND
-        ONE JOB ONLY: PROVIDE A JSON OBJECT OF FORM VALUES THAT WILL BE USED TO
-        FILL THE GENERATE UI IN THE EDITOR. The generate UI is a form that
-        allows users to select options for a generator. <br />
-        The user is trying to use the {this.props.generatorName} generator.
-      </UserMessage>
+      <>
+        <UserMessage priority={100}>
+          You are an AI assistant specialized in Nx workspaces and monorepo
+          development. You provide precise, technical guidance for developers
+          working with Nx tools, patterns, and best practices. Use the provided
+          tools available to you to answer the users query.
+          <br /> YOU HAVE ONE JOB AND ONE JOB ONLY: PROVIDE A JSON OBJECT OF
+          FORM VALUES THAT WILL BE USED TO FILL THE GENERATE UI IN THE EDITOR.
+          The generate UI is a form that allows users to select options for a
+          generator. <br />
+          The user is trying to use the {this.props.generatorName} generator.
+          <br />
+          The current form values are: {JSON.stringify(this.props.formValues)}
+          <br />
+          You can make changes to them or overwrite them but leave those that
+          aren't relevant to the query the same.
+        </UserMessage>
+        <UserMessage priority={80}>
+          This is the schema for the generator:{' '}
+          {JSON.stringify(this.props.generatorSchema)}
+        </UserMessage>
+        <DocsPagesPrompt
+          docsPages={this.props.docsPages}
+          flexGrow={5}
+          passPriority
+        />
+        <UserMessage priority={90}>{this.props.userQuery}</UserMessage>
+      </>
     );
   }
 }
