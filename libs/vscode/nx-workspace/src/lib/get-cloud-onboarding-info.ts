75227075e512e78cf00f374f5141443fe5ca058e
MaxKless <34165455+MaxKless@users.noreply.github.com>
2025-08-05 11:11:45 +0200

    fix: improve misc cloud onboarding things  (#2656)

--- libs/vscode/nx-workspace/src/lib/get-cloud-onboarding-info.ts
+++ libs/vscode/nx-workspace/src/lib/get-cloud-onboarding-info.ts
@@ -2,8 +2,8 @@ import { NxCloudOnboardingInfoRequest } from '@nx-console/language-server-types'
 import { CloudOnboardingInfo } from '@nx-console/shared-types';
 import { getNxlsClient } from '@nx-console/vscode-lsp-client';
 
-export async function getCloudOnboardingInfo(): Promise<
-  CloudOnboardingInfo | undefined
-> {
-  return getNxlsClient().sendRequest(NxCloudOnboardingInfoRequest, undefined);
+export async function getCloudOnboardingInfo(
+  force = false,
+): Promise<CloudOnboardingInfo | undefined> {
+  return getNxlsClient().sendRequest(NxCloudOnboardingInfoRequest, { force });
 }
