45de61f2bd41387bde4af2e50abf2312c2ce801c
Max Kless <maxk@nrwl.io>
2025-08-13 13:47:55 +0200

    dont set initialCommand, add initialized handler instead

--- libs/vscode/project-graph/src/init-vscode-project-graph.ts
+++ libs/vscode/project-graph/src/init-vscode-project-graph.ts
@@ -49,11 +49,12 @@ export async function initVscodeProjectGraph(context: ExtensionContext) {
     legacyGrapyWebView,
     // Temporary simple command to open the new state-machine powered graph webview
     commands.registerCommand('nx.graph.new', async () => {
-      const newGraphWebviewManager = new NewGraphWebviewManager({
+      const newGraphWebviewManager = new NewGraphWebviewManager();
+      newGraphWebviewManager.reveal();
+      await newGraphWebviewManager.sendCommandToGraph({
         type: 'showAll',
         autoExpand: true,
       });
-      newGraphWebviewManager.reveal();
       // await setTimeout(10000);
       // const result = await newGraphWebviewManager.sendCommandToGraph({
       //   type: 'excludeNode',
