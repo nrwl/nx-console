fe4f504c2584a70309cd5b72110b0e95b8dae5f7
Max Kless <maxk@nrwl.io>
2025-09-02 19:29:17 +0200

    also check provenance contents

--- libs/shared/utils/src/lib/provenance.ts
+++ libs/shared/utils/src/lib/provenance.ts
@@ -1,66 +1,46 @@
 import { exec } from 'node:child_process';
-import { verify, VerifyOptions } from 'sigstore';
 import { promisify } from 'node:util';
-import { xhr } from 'request-light';
-import { createHash } from 'node:crypto';
-
-const pacote = require('pacote');
 
 export async function nxLatestHasProvenance(): Promise<boolean> {
   try {
     const npmView = (
-      await promisify(exec)(`npm view nx@latest dist --json`, {
+      await promisify(exec)(`npm view nx@next --json`, {
         encoding: 'utf-8',
       })
     ).stdout.trim();
 
-    const dist = JSON.parse(npmView);
-    const attURL: string | undefined = dist?.attestations?.url;
-    const tarballURL: string | undefined = dist?.tarball;
+    const npmViewResult = JSON.parse(npmView);
+    const attURL: string | undefined = npmViewResult.dist?.attestations?.url;
 
-    if (!attURL || !tarballURL) return false;
+    if (!attURL) return false;
 
-    const [attestations, tarball] = await Promise.all([
-      getJSON<any>(attURL),
-      getBinary(tarballURL),
-    ]);
+    const attestations = (await (await fetch(attURL)).json()) as any;
 
-    pacote;
-
-    const bundle = attestations?.attestations?.find(
+    const provenanceBundle = attestations?.attestations?.find(
       (a) => a.predicateType === 'https://slsa.dev/provenance/v1',
     ).bundle;
 
-    const sha = createHash('sha512').update(tarball).digest('hex');
-    const payload = JSON.parse(atob(bundle.dsseEnvelope.payload));
+    const dsseEnvelopePayload = JSON.parse(
+      atob(provenanceBundle.dsseEnvelope.payload),
+    );
 
-    const result = await pacote.manifest(`nx@21.4.1`, {
-      verifySignatures: true,
-      verifyAttestations: true,
-    });
+    const workflowParameters =
+      dsseEnvelopePayload?.predicate?.buildDefinition?.externalParameters
+        ?.workflow;
 
-    const verifyOpts: VerifyOptions = {
-      certificateIssuer: 'https://token.actions.githubusercontent.com',
-    };
-
-    await verify(bundle, verifyOpts);
+    if (workflowParameters?.repository !== 'https://github.com/nrwl/nx') {
+      return false;
+    }
+    if (workflowParameters?.path !== '.github/workflows/publish.yml') {
+      return false;
+    }
+    if (workflowParameters?.ref !== `refs/tags/${npmViewResult.version}`) {
+      return false;
+    }
     return true;
   } catch (e) {
     return false;
   }
-}
-
-async function getJSON<T = unknown>(url: string): Promise<T> {
-  const r = await fetch(url);
-  if (!r.ok) throw new Error(`GET ${url} -> ${r.status}`);
-  return (await r.json()) as T;
-}
-
-async function getBinary(url: string): Promise<Buffer> {
-  const r = await fetch(url);
-  if (!r.ok) throw new Error(`GET ${url} -> ${r.status}`);
-  const ab = await r.arrayBuffer();
-  return Buffer.from(ab);
 }
 
 export const noProvenanceError = `An error occurred while checking the integrity of the latest version of Nx. This shouldn't happen. Please file an issue at https://github.com/nrwl/nx-console/issues`;
