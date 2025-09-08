0a125c28c404bb65372759ddb1fa188853209c10
nkwanui237 <nkwanuigracious3@gmail.com>
2025-09-08 09:06:10 -0700

    Delete tools directory

--- tools/scripts/clear-dist.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-
-fs.rmSync('./dist/apps/vscode', { recursive: true, force: true });
--- tools/scripts/copy-nxls-package-json.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const { execSync } = require('child_process');
-fs.copySync('./apps/nxls/package.json', './dist/apps/nxls/package.json');
-
-execSync(`npm i -f --install-strategy nested`, {
-  stdio: 'inherit',
-  cwd: './dist/apps/nxls',
-});
