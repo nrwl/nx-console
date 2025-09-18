26be2a33993cbe19b9949dc79b0a2fd2ebdf6989
lourw <56288712+lourw@users.noreply.github.com>
2025-09-18 09:44:17 -0700

    chore(repo): remove build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,3 @@ rootProject.name = "nx-console"
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0" }
 
 includeBuild("apps/intellij")
-
-includeBuild("/Users/lourw/nrwl/nx/packages/gradle/project-graph")
-
