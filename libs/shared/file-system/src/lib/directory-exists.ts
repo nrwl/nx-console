87dd2d0203b5c376909bd93996c89b7a0679cb42
Emily M Klassen <760204+forivall@users.noreply.github.com>
2025-09-25 14:30:03 -0700

    fix(nxls): misc fixes to get nxls to work with helix

--- libs/shared/file-system/src/lib/directory-exists.ts
+++ libs/shared/file-system/src/lib/directory-exists.ts
@@ -1,8 +1,9 @@
+import { URI } from 'vscode-uri';
 import { stat } from 'fs/promises';
 
 export async function directoryExists(filePath: string): Promise<boolean> {
   try {
-    return (await stat(filePath)).isDirectory();
+    return (await stat(URI.parse(filePath).fsPath)).isDirectory();
   } catch {
     return false;
   }
