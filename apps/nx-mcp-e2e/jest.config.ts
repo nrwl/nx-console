4c680bdf14d2b3cff6a58caff546201b525d9e6e
Jason Jean <jasonjean1993@gmail.com>
2025-12-19 16:37:02 -0500

    chore: [nx migration] convert-jest-config-to-cjs

--- apps/nx-mcp-e2e/jest.config.ts
+++ apps/nx-mcp-e2e/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'nx-mcp-e2e',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
--- libs/nx-mcp/nx-mcp-server/jest.config.ts
+++ libs/nx-mcp/nx-mcp-server/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'nx-mcp-server',
   preset: '../../../jest.preset.js',
   globals: {},
