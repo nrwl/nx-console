108ce2f2f81fb7f657ee033915459bee8fe44b13
Jason Jean <jasonjean1993@gmail.com>
2026-07-05 15:20:49 -0400

    chore(repo): [nx migration] update-23-1-0-convert-to-flat-config

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import nx from '@nx/eslint-plugin';
+
+export default [
+  ...nx.configs['flat/base'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: 'type:vscode',
+              onlyDependOnLibsWithTags: [
+                'type:shared',
+                'type:vscode',
+                'type:mcp',
+              ],
+            },
+            {
+              sourceTag: 'type:lsp',
+              onlyDependOnLibsWithTags: ['type:lsp', 'type:shared', 'type:mcp'],
+              bannedExternalImports: ['vscode'],
+            },
+            {
+              sourceTag: 'type:mcp',
+              onlyDependOnLibsWithTags: ['type:mcp', 'type:shared'],
+              bannedExternalImports: ['vscode'],
+            },
+            {
+              sourceTag: 'type:shared',
+              onlyDependOnLibsWithTags: ['type:shared'],
+              bannedExternalImports: ['vscode'],
+            },
+            {
+              sourceTag: 'type:generate-ui',
+              onlyDependOnLibsWithTags: ['type:shared', 'type:generate-ui'],
+            },
+          ],
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          name: '@nx/devkit',
+          message:
+            'Please use nx/src/devkit-exports instead, or import function directly from @nx/devkit/src/*',
+        },
+        {
+          name: 'semver',
+          importNames: ['gte', 'gt'],
+          message: 'Please use @nx-console/nx-version instead',
+        },
+      ],
+    },
+  },
+  ...nx.configs['flat/typescript'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_$',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-extra-semi': 'error',
+      'no-extra-semi': 'off',
+    },
+  },
+  ...nx.configs['flat/javascript'],
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      '@typescript-eslint/no-extra-semi': 'error',
+      'no-extra-semi': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    // Override or add rules here
+    rules: {},
+  },
+];
